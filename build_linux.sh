30bdf95e51cb99396d4710f1401ac0a136c12c99
Cheng Li <scrappedprince.li@gmail.com>
2017-12-24 12:15:00 +0800

    using more automatic script to install

--- build_linux.sh
+++ build_linux.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 export num_cores=`grep -c processor /proc/cpuinfo`
+export dal_dir=$PWD
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$dal_dir ..
 make clean
 make -j${num_cores}
+make install
 
 cd ..
 bin/test_suite
