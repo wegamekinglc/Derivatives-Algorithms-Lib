df8cac0a490a8e934ff026a0915b496da39294ee
Cheng Li <scrappedprince.li@gmail.com>
2017-12-24 12:13:36 +0800

    using more automatic script to install

--- build_windows.bat
+++ build_windows.bat
@@ -1,6 +1,14 @@
 @echo off
 
 set BUILD_TYPE=Release
+set DAL_DIR=%CD%
+set ADDRESS_MODEL=Win64
+
+if %ADDRESS_MODEL%==Win64 (
+  set PLATFORM=x64
+) else (
+  set PLATFORM=Win32
+)
 
 if exist build (
   rem build folder already exists.
@@ -10,11 +18,16 @@ if exist build (
 
 cd build
 
-cmake -G "Visual Studio 14 2015 Win64" ..
+if %ADDRESS_MODEL%==Win64 (
+  cmake -G "Visual Studio 14 2015 %ADDRESS_MODEL%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%DAL_DIR% --target install ..
+) else (
+  cmake -G "Visual Studio 14 2015" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%DAL_DIR% --target install ..
+)
 
 if %errorlevel% neq 0 exit /b 1
 
-msbuild dal.sln /m /p:Configuration=%BUILD_TYPE% /p:Platform=x64
+msbuild dal.sln /m /p:Configuration=%BUILD_TYPE% /p:Platform=%PLATFORM%
+msbuild INSTALL.vcxproj /m:%NUMBER_OF_PROCESSORS% /p:Configuration=%BUILD_TYPE% /p:Platform=%PLATFORM%
 
 if %errorlevel% neq 0 exit /b 1
 
