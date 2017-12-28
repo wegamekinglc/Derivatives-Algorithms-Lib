b409f7b71c1cf77cab864f74a6bb10eec5b800aa
Cheng Li <scrappedprince.li@gmail.com>
2017-12-28 22:57:07 +0800

    added some explanation

--- dal/utilities/asserts.hpp
+++ dal/utilities/asserts.hpp
@@ -11,23 +11,27 @@
 
 namespace dal {
 
-    //! Base error class
     class Error : public std::exception {
     public:
-        /*! The explicit use of this constructor is not advised.
-            Use the QL_FAIL macro instead.
-        */
+        /*
+         * The explicit use of this constructor is not advised.
+         * Use the DAL_ASSERT macro instead.
+         */
         Error(const std::string& file,
               long line,
               const std::string& functionName,
               const std::string& message = "");
-        /*! the automatically generated destructor would
-            not have the throw specifier.
-        */
+        /*
+         * the automatically generated destructor would
+         * not have the throw specifier.
+         */
         ~Error() noexcept override = default;
-        //! returns the error message.
         const char* what() const noexcept override;
     private:
+        /*
+         * can't use std::string directly
+         * the copy constructor for std::string is not declared noexcept
+         */
         std::shared_ptr<std::string> message_;
     };
 
