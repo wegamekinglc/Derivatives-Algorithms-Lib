99d1265757dad3ac86f8159b46c14e2a5d5c4da7
wegamekinglc <scrappedprince.li@gmail.com>
2017-12-29 10:43:14 +0800

    made it possible to link with static msvc runtime

--- build_windows.bat
+++ build_windows.bat
@@ -3,6 +3,7 @@
 set BUILD_TYPE=Release
 set DAL_DIR=%CD%
 set ADDRESS_MODEL=Win64
+set MSVC_RUNTIME=dynamic
 
 if %ADDRESS_MODEL%==Win64 (
   set PLATFORM=x64
@@ -19,9 +20,9 @@ if exist build (
 cd build
 
 if %ADDRESS_MODEL%==Win64 (
-  cmake -G "Visual Studio 14 2015 %ADDRESS_MODEL%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%DAL_DIR% --target install ..
+  cmake -G "Visual Studio 14 2015 %ADDRESS_MODEL%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%DAL_DIR% -DMSVC_RUNTIME=%MSVC_RUNTIME% --target install ..
 ) else (
-  cmake -G "Visual Studio 14 2015" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%DAL_DIR% --target install ..
+  cmake -G "Visual Studio 14 2015" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%DAL_DIR% -DMSVC_RUNTIME=%MSVC_RUNTIME% --target install ..
 )
 
 if %errorlevel% neq 0 exit /b 1
