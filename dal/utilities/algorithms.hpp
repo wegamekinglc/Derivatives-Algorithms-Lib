be250340de34e5e53415b521295876682609d50a
Cheng Li <scrappedprince.li@gmail.com>
2018-01-01 12:59:46 +0800

    fixed type

--- dal/utilities/algorithms.hpp
+++ dal/utilities/algorithms.hpp
@@ -25,7 +25,7 @@ template<class CS_, class OP_, class CD_> void Transform(const CS_& src, OP_ op,
 
 template <typename C, typename OP> auto Apply(OP op, const C& src)->Vector_<decltype(op(*src.begin()))>
 {
-    using vector_t = Vector_<std::remove_const<decltype(op(*src.begin()))>::type>;
+    using vector_t = Vector_<decltype(op(*src.begin()))>;
     vector_t ret_val(src.size());
     Transform(src, op, &ret_val);
     return ret_val;
