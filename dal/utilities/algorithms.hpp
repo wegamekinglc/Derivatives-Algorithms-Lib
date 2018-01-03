4580f5b3dc3083925d7ad715d6f8a78b80934dde
Cheng Li <scrappedprince.li@gmail.com>
2018-01-04 00:02:08 +0800

    added binary transform

--- dal/utilities/algorithms.hpp
+++ dal/utilities/algorithms.hpp
@@ -5,9 +5,9 @@
 #pragma once
 
 #include <algorithm>
-#include <type_traits>
 #include <dal/platform/platform.hpp>
 #include <dal/utilities/asserts.hpp>
+#include <type_traits>
 
 #define ASSIGN(p, v)                                                                                                   \
     if (!(p))                                                                                                          \
@@ -15,34 +15,34 @@
     else                                                                                                               \
         *(p) = (v)
 
-
-template<class CS_, class OP_, class CD_> void Transform(const CS_& src, OP_ op, CD_* dst)
-{
+template <class CS_, class OP_, class CD_> void Transform(const CS_& src, OP_ op, CD_* dst) {
     DAL_ASSERT(dst && src.size() == dst->size(), "dst is null or src size is not compatible with dst size");
     std::transform(src.begin(), src.end(), dst->begin(), op);
 }
 
+template <class CS1_, class CS2_, class OP_, class CD_>
+void Transform(const CS1_& src1, const CS2_& src2, OP_ op, CD_* dst) {
+    DAL_ASSERT(dst && src1.size() == dst->size() && src2.size() == dst->size(),
+               "dst is null or src size is not compatible with dst size");
+    std::transform(src1.begin(), src1.end(), src2.begin(), dst->begin(), op);
+};
 
-template <typename C, typename OP> auto Apply(OP op, const C& src)->Vector_<decltype(op(*src.begin()))>
-{
+template <typename C, typename OP>
+auto Apply(OP op, const C& src) -> Vector_<std::remove_reference_t<std::remove_const_t<decltype(op(*src.begin()))>>> {
     using vector_t = Vector_<std::remove_reference_t<std::remove_const_t<decltype(op(*src.begin()))>>>;
     vector_t ret_val(src.size());
     Transform(src, op, &ret_val);
     return ret_val;
 }
 
-
-template <class CS_, class CD_>  void Copy(const CS_& src, CD_* dst)
-{
+template <class CS_, class CD_> void Copy(const CS_& src, CD_* dst) {
     DAL_ASSERT(dst && src.size() == dst->size(), "dst is null or src size is not compatible with dst size");
     std::copy(src.begin(), src.end(), dst->begin());
 };
 
-
-template <class C_> Vector_<typename C_::value_type> Copy(const C_& src)
-{
+template <class C_> Vector_<typename C_::value_type> Copy(const C_& src) {
     using vector_t = Vector_<typename C_::value_type>;
-    vector_t  ret_val(src.size());
+    vector_t ret_val(src.size());
     std::copy(src.begin(), src.end(), ret_val.begin());
     return ret_val;
 }
--- tests/utilities/algorithms.cpp
+++ tests/utilities/algorithms.cpp
@@ -2,32 +2,40 @@
 // Created by Cheng Li on 2017/12/29.
 //
 
-#include <functional>
 #include <dal/platform/platform.hpp>
-#include <dal/utilities/algorithms.hpp>
 #include <dal/math/vector.hpp>
+#include <dal/utilities/algorithms.hpp>
+#include <functional>
 #include <gtest/gtest.h>
 
+TEST(AlgorothmsTest, TestTransformWithUniaryOPerator) {
+    Vector_<> s = {1, 2, 3};
+    Vector_<> d(3);
 
-TEST(AlgorothmsTest, TestTransformWithContainer) {
-    Vector_<> s1 = {1, 2, 3};
-    Vector_<> s2(3);
+    Transform(s, std::negate<>(), &d);
 
-    Transform(s1, std::negate<>(), &s2);
+    for (int i = 0; i != d.size(); ++i) {
+        ASSERT_DOUBLE_EQ(s[i], -d[i]);
+    }
+}
 
-    for(int i=0; i != s2.size(); ++i) {
-        ASSERT_DOUBLE_EQ(s2[i], -s1[i]);
+TEST(AlgorothmsTest, TestTransformWithBinaryOperator) {
+    Vector_<> s = {1, 2, 3};
+    Vector_<> d(s.size());
+
+    Transform(s, s, std::plus<>(), &d);
+    for (int i = 0; i != d.size(); ++i) {
+        ASSERT_DOUBLE_EQ(s[i] + s[i], d[i]);
     }
 }
 
 TEST(AlgorothmsTest, TestApply) {
     Vector_<> s1 = {1, 2, 3};
     auto s2 = Apply(std::negate<>(), s1);
-    for(int i=0; i != s2.size(); ++i) {
+    for (int i = 0; i != s2.size(); ++i) {
         ASSERT_DOUBLE_EQ(s2[i], -s1[i]);
     }
 }
-
 
 TEST(AlgorothmsTest, TestCopyWithPreAllocatedVector) {
     Vector_<int> s1 = {1, 2, 3};
@@ -35,18 +43,17 @@ TEST(AlgorothmsTest, TestCopyWithPreAllocatedVector) {
 
     Copy(s1, &s2);
 
-    for(auto i=0; i != s1.size(); ++i) {
+    for (auto i = 0; i != s1.size(); ++i) {
         ASSERT_EQ(s1[i], s2[i]);
     }
 }
-
 
 TEST(AlgorothmsTest, TestCopyWithReturnedValue) {
     Vector_<int> s1 = {1, 2, 3};
 
     auto s2 = Copy(s1);
 
-    for(auto i=0; i != s1.size(); ++i) {
+    for (auto i = 0; i != s1.size(); ++i) {
         ASSERT_EQ(s1[i], s2[i]);
     }
 }
