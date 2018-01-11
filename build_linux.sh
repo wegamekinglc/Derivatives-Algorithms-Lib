a6642b09b17ba7da32597f64a3aa7e0fa247cad3
wegamekinglc <scrappedprince.li@gmail.com>
2018-01-11 17:44:05 +0800

    added more

--- build_linux.sh
+++ build_linux.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 export num_cores=`grep -c processor /proc/cpuinfo`
-export dal_dir=$PWD
+export DAL_DIR=$PWD
 export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$dal_dir ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DAL_DIR ..
 make clean
 make -j${num_cores}
 make install
