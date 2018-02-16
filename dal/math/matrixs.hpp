1f9b82271ac48fae5b34802ae12c544b010f7e60
wegamekinglc <scrappedprince.li@gmail.com>
2018-02-17 00:21:51 +0800

    added more matrix functionality

--- dal/math/matrixs.hpp
+++ dal/math/matrixs.hpp
@@ -35,6 +35,51 @@ namespace dal {
         int Cols() const { return cols_;}
         bool Empty() const { return vals_.empty();}
         void Clear() { vals_.clear(); cols_ = 0; hooks_.clear();}
+        CI_ First() const { return vals_.begin(); }
+        CI_ Last() const { return  vals_.end();}
+
+        CR_ operator()(int row, int col) const { return hooks_[row][col];}
+        R_ operator()(int row, int col) { return hooks_[row][col];}
+
+        // slices -- ephemeral containers of rows or columns
+        class ConstRow_ {
+        protected:
+            I_ begin_;
+            I_ end_;
+        public:
+            using value_type = E_;
+            using const_iterator = typename Vector_<E_>::const_iterator;
+
+            ConstRow_(I_ begin, I_ end): begin_(begin), end_(end) {}
+            ConstRow_(I_ begin, int size): begin_(begin), end_(begin + size) {}
+
+            const_iterator begin() const { return begin_; }
+            const_iterator end() const { return end_; }
+            int size() const { return static_cast<int>(end_ - begin_);}
+            const E_& operator()(int col) const { return *(begin_ + col);}
+            const E_& front() const { return *begin_;}
+            const E_& back() const { return *(end_ - 1);}
+        };
+
+        ConstRow_ Row(int i_row) const { return ConstRow_(hooks_[i_row], cols_);}
+        ConstRow_ operator[](int i_row) const { return Row(i_row);}
+
+        struct Row_ : ConstRow_ {
+            using iterator = I_;
+            Row_(I_ begin, I_ end): ConstRow_(begin, end) {}
+            Row_(I_ begin, int size): ConstRow_(begin, size) {}
+
+            // have to double-implement begin/end, otherwise non-const implementations hide the inherited const
+            iterator begin() { return begin_;}
+            const_iterator begin() const { return begin_;}
+            iterator end() { return ConstRow_::end_;}
+            const_iterator end() const { return end_;}
+            E_& operator[](int col) { return *(begin_ + col);}
+            const E_& operator()(int col) const { return *(begin_ + col);}
+        };
+
+        Row_ Row(int i_row) { return Row_(hooks_[i_row], cols_);}
+        Row_ operator[](int i_row) { return Row(i_row);}
     };
 
     template <class E_>
--- tests/math/matrixs.cpp
+++ tests/math/matrixs.cpp
@@ -46,3 +46,21 @@ TEST(MatrixTest, TestMatrixClear) {
     ASSERT_EQ(m1.Rows(), 0);
     ASSERT_EQ(m1.Cols(), 0);
 }
+
+TEST(MatrixTest, TestMatrixFirstLast) {
+    matrix_t m1(3, 4);
+    ASSERT_EQ(3 * 4, m1.Last() - m1.First());
+}
+
+TEST(MatrixText, TestMatrixConstAccess) {
+    matrix_t m1(2, 2);
+    m1(1, 0) = 2.;
+    const matrix_t m2(m1);
+    ASSERT_DOUBLE_EQ(2., m2(1, 0));
+}
+
+TEST(MatrixTest, TestMatrixAccess) {
+    matrix_t m1(2, 2);
+    m1(1, 0) = 2.;
+    ASSERT_DOUBLE_EQ(2., m1(1, 0));
+}
