a4c193ad116d034a783ee7b31ddc8b351789727d
wegamekinglc <scrappedprince.li@gmail.com>
2019-10-26 21:39:56 +0800

    added build_type

--- build_linux.sh
+++ build_linux.sh
@@ -3,10 +3,11 @@
 export num_cores=`grep -c processor /proc/cpuinfo`
 export DAL_DIR=$PWD
 export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
+export BUILD_TYPE=Release
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DAL_DIR ..
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$DAL_DIR ..
 make clean
 make -j${num_cores}
 make install
