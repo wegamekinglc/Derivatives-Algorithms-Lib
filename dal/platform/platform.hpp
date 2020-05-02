218994ae3588165d20f64c82977e4ababca3af93
wegamekinglc <scrappedprince.li@gmail.com>
2020-05-02 19:56:09 +0800

    FIX: do not use boost

--- dal/platform/platform.hpp
+++ dal/platform/platform.hpp
@@ -8,7 +8,7 @@
 #include <dal/platform/strict.hpp>
 #include <memory>
 #include <utility>
-#include <boost/noncopyable.hpp>
+#include <dal/utilities/noncopyable.hpp>
 
 #ifdef INFINITY
 #undef INFINITY
@@ -22,7 +22,6 @@
 #undef MAX
 #endif
 
-using boost::noncopyable;
 using std::make_pair;
 using std::pair;
 
