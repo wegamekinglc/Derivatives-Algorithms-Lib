0d67ecd44b6475c90220685eb1abf3c6d03b2d67
wegamekinglc <scrappedprince.li@gmail.com>
2020-11-09 16:08:00 +0800

    FIX: terminate where faild

--- build_linux.sh
+++ build_linux.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -e
 
 export num_cores=`grep -c processor /proc/cpuinfo`
 export DAL_DIR=$PWD
@@ -12,18 +12,8 @@ cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$DAL_DIR ..
 make clean
 make -j${num_cores}
 make install
-) || ret=$?
+)
 
-if [ $ret -ne 0 ]; then
-    echo "Error in building"
-    exit 1
-fi
-
-bin/test_suite || ret=$?
-
-if [ $ret -ne 0 ]; then
-    echo "Error in testing"
-    exit 1
-fi
+bin/test_suite
 
 echo "Finished building of Derivatives Algorithms Library"
