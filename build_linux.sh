be76f18cf91fd44dc9cc7f09158c88356924d10a
wegamekinglc <scrappedprince.li@gmail.com>
2020-11-09 15:42:07 +0800

    FIX: using transparent less_than<>

--- build_linux.sh
+++ build_linux.sh
@@ -6,13 +6,14 @@ export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
 export BUILD_TYPE=Debug
 
 mkdir build
-cd build
+(
+cd build || exit
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$DAL_DIR ..
 make clean
 make -j${num_cores}
 make install
+)
 
-cd ..
 bin/test_suite
 
 echo "Finished building of Derivatives Algorithms Library"
--- dal/math/interp.cpp
+++ dal/math/interp.cpp
@@ -6,12 +6,41 @@
 #include <dal/math/interp.hpp>
 #include <dal/platform/strict.hpp>
 
+#include <dal/utilities/algorithms.hpp>
+#include <dal/storage/archive.hpp>
+
 namespace Dal {
     Interp1_::Interp1_(const String_& name) :Storable_("Interp1", name) {}
 
     Interp1Linear_::Interp1Linear_(const String_& name, const Vector_<>& x, const Vector_<>& f)
     :Interp1_(name), x_(x), f_(f) {
         REQUIRE(x_.size() == f_.size(), "x_ size must be equal to f_ size");
-        REQUIRE(IsMonotonic(x, std::less_equal()), "x_ array should be monotonic");
+        REQUIRE(IsMonotonic(x_, std::less_equal()), "x_ array should be monotonic");
     }
+
+    Interp1Linear_::Interp1Linear_(const String_& name, const std::map<double, double>& f)
+    : Interp1_(name), x_(Keys(f)), f_(MapValues(f)) {
+        REQUIRE(IsMonotonic(x_, std::less_equal<>()), "x_ array should be monotonic");
+    }
+
+    double Interp1Linear_::operator()(double x) const {
+        auto pge = LowerBound(x_, x);
+        if(pge == x_.end())
+            return f_.back();
+        else if(pge == x_.begin() || IsZero(x - *pge))
+            return f_[pge - x_.begin()];
+        else {
+            auto plt = Previous(pge);
+            const double gFrac = (x - *plt) / (*pge - *plt);
+            auto flt = f_.begin() + (plt - x_.begin());
+            return *flt + gFrac * (*Next(flt) - *flt);
+        }
+    }
+
 }
+
+//namespace {
+//    #include <dal/auto/MG_Interp1Linear_v1_Read.inc>
+//    #include <dal/auto/MG_Interp1Linear_v1_Write.inc>
+//}
+
