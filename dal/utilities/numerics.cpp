37ebea195899748a0cda80615af86cbe3e2ad35f
wegamekinglc <scrappedprince.li@gmail.com>
2020-11-16 02:22:06 +0800

    FIX: added cmath

--- dal/utilities/numerics.cpp
+++ dal/utilities/numerics.cpp
@@ -2,22 +2,24 @@
 // Created by wegam on 2020/11/16.
 //
 
+#include <dal/platform/platform.hpp>
 #include <dal/utilities/numerics.hpp>
 #include <dal/utilities/exceptions.hpp>
+#include <cmath>
 
 namespace Dal {
     int AsInt(double src) {
-        REQUIRE(fabs(src) < 1e9, "Number is too large to be an integer");
+        REQUIRE(std::fabs(src) < 1e9, "Number is too large to be an integer");
         return static_cast<int>(src + (src > 0 ? 1e-9 : -1e-9));
     }
 
     int NearestInt(double src) {
-        REQUIRE(fabs(src) < 1e9, "Number is too large to be an integer");
+        REQUIRE(std::fabs(src) < 1e9, "Number is too large to be an integer");
         return static_cast<int>(src + (src > 0 ? 0.5 : -0.5));
     }
 
     int AsInt(std::ptrdiff_t src) {
-        REQUIRE(abs(src) < 1e9, "32-bit integer overflow");
+        REQUIRE(std::abs(src) < 1e9, "32-bit integer overflow");
         return static_cast<int>(src);
     }
 }
