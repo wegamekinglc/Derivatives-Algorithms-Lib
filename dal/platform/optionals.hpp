032cf50a8d545b1e219dff8ab70e5b1726ade3f7
wegamekinglc <scrappedprince.li@gmail.com>
2020-11-23 17:20:01 +0800

    FIX: some format change

--- dal/platform/optionals.hpp
+++ dal/platform/optionals.hpp
@@ -16,5 +16,7 @@ const T_& operator+(const boost::optional<T_>& opt) {
 // construct from a pointer
 template<class T_>
 boost::optional<T_> as_optional(const T_* p) {
-    boost::optional<T_> retval; if (p) retval = *p; return retval;
+    boost::optional<T_> ret_val;
+    if (p)
+        ret_val = *p; return ret_val;
 }
