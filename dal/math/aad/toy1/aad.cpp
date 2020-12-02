c8e3c81a484a9fdf89ed58cfa4d907a7ece30b19
wegamekinglc <scrappedprince.li@gmail.com>
2020-12-03 02:31:21 +0800

    FEATURE: added first version of add

--- dal/math/aad/toy1/aad.cpp
+++ dal/math/aad/toy1/aad.cpp
@@ -2,3 +2,218 @@
 // Created by wegam on 2020/12/2.
 //
 
+#include <dal/math/aad/toy1/aad.hpp>
+#include <iostream>
+#include <cmath>
+
+namespace Dal {
+    double Node_::Result() const {
+        return result_;
+    }
+
+    void Node_::ResetProcessed() {
+        for (const auto& arg : arguments_)
+            arg->ResetProcessed();
+        processed_ = false;
+    }
+
+    void Node_::SetOrder(unsigned int order) {
+        order_ = order;
+    }
+
+    unsigned Node_::Order() const {
+        return order_;
+    }
+
+    double& Node_::Adjoint() {
+        return adjoint_;
+    }
+
+    void Node_::ResetAdjoints() {
+        for (const auto& arg : arguments_)
+            arg->ResetAdjoints();
+        adjoint_ = 0.;
+    }
+
+    PlusNode_::PlusNode_(const shared_ptr<Node_>& lhs, const shared_ptr<Node_>& rhs) {
+        arguments_.Resize(2);
+        arguments_[0] = lhs;
+        arguments_[1] = rhs;
+    }
+
+    /*
+     * Plus nodes stuff
+     */
+
+    void PlusNode_::Evaluate() {
+        result_ = arguments_[0]->Result() + arguments_[1]->Result();
+    }
+
+    void PlusNode_::LogInstruction() {
+        std::cout << "y" << order_ << " = y"
+                  << arguments_[0]->Order() << " + y"
+                  << arguments_[1]->Order() << std::endl;
+    }
+
+    void PlusNode_::PropagateAdjoint() {
+        std::cout << "Propagation node " << order_
+                  << " adjoint = " << adjoint_ << std::endl;
+        arguments_[0]->Adjoint() += adjoint_;
+        arguments_[1]->Adjoint() += adjoint_;
+        adjoint_ = 0.;
+    }
+
+    /*
+     * Times nodes stuff
+     */
+
+    TimesNode_::TimesNode_(const shared_ptr<Node_>& lhs, const shared_ptr<Node_>& rhs) {
+        arguments_.Resize(2);
+        arguments_[0] = lhs;
+        arguments_[1] = rhs;
+    }
+
+    void TimesNode_::Evaluate() {
+        result_ = arguments_[0]->Result() * arguments_[1]->Result();
+    }
+
+    void TimesNode_::LogInstruction() {
+        std::cout << "y" << order_ << " = y"
+                  << arguments_[0]->Order() << " * y"
+                  << arguments_[1]->Order() << std::endl;
+    }
+
+    void TimesNode_::PropagateAdjoint() {
+        std::cout << "Propagation node " << order_
+                  << " adjoint = " << adjoint_ << std::endl;
+        arguments_[0]->Adjoint() += arguments_[1]->Result() * adjoint_;
+        arguments_[1]->Adjoint() += arguments_[0]->Result() * adjoint_;
+        adjoint_ = 0.;
+    }
+
+    /*
+     * Log nodes stuff
+     */
+
+    LogNode_::LogNode_(const shared_ptr<Node_>& arg) {
+        arguments_.Resize(1);
+        arguments_[0] = arg;
+    }
+
+    void LogNode_::Evaluate() {
+        result_ = std::log(arguments_[0]->Result());
+    }
+
+    void LogNode_::LogInstruction() {
+        std::cout << "y" << order_ << " = log(y"
+                  << arguments_[0]->Order() << ")" << std::endl;
+    }
+
+    void LogNode_::PropagateAdjoint() {
+        std::cout << "Propagation node " << order_
+                  << " adjoint = " << adjoint_ << std::endl;
+        arguments_[0]->Adjoint() += adjoint_ / arguments_[0]->Result();
+        adjoint_ = 0.;
+    }
+
+    /*
+     * Leaf nodes stuff
+     */
+
+    Leaf_::Leaf_(double val) : value_(val) {}
+
+    double Leaf_::GetVal() const {
+        return value_;
+    }
+
+    void Leaf_::SetVal(double val) {
+        value_ = val;
+    }
+
+    void Leaf_::Evaluate() {
+        result_ = value_;
+    }
+
+    void Leaf_::LogInstruction() {
+        std::cout << "y" << order_ << " = " << value_ << std::endl;
+    }
+
+    void Leaf_::PropagateAdjoint() {
+        std::cout << "Accumulating leaf " << order_
+                  << " adjoint = " << adjoint_ << std::endl;
+    }
+
+    /*
+     * Number class stuff
+     */
+
+    Number_::Number_(double val) : node_(new Leaf_(val)) {}
+
+    Number_::Number_(const shared_ptr<Node_>& node) : node_(node) {}
+
+    shared_ptr<Node_> Number_::Node() const {
+        return node_;
+    }
+
+    void Number_::SetVal(double val) {
+        std::dynamic_pointer_cast<Leaf_>(node_)->SetVal(val);
+    }
+
+    double Number_::GetVal() const {
+        return std::dynamic_pointer_cast<Leaf_>(node_)->GetVal();
+    }
+
+    double Number_::Evaluate() {
+        node_->ResetProcessed();
+        auto func = [](Node_& n) { n.Evaluate(); };
+        node_->PostOrder(func);
+        return node_->Result();
+    }
+
+    void Number_::SetOrder() {
+        node_->ResetProcessed();
+        auto func = [order = 0](Node_& n) mutable { n.SetOrder(++order); };
+        node_->PostOrder(func);
+    }
+
+    void Number_::LogResult() const {
+        node_->ResetProcessed();
+        auto func = [](Node_& n) {
+            std::cout << "Processed node " << n.Order() << " result " << n.Result() << std::endl;
+        };
+        node_->PostOrder(func);
+    }
+
+    void Number_::LogProgram() {
+        node_->ResetProcessed();
+        auto func = [](Node_& n) { n.LogInstruction(); };
+        node_->PostOrder(func);
+    }
+
+    double& Number_::Adjoint() {
+        return node_->Adjoint();
+    }
+
+    void Number_::PropagateAdjoints() {
+        node_->ResetAdjoints();
+        node_->Adjoint() = 1.0;
+        auto func = [](Node_& n) { n.PropagateAdjoint();};
+        node_->PreOrder(func);
+    }
+
+    /*
+     * Operators stuff
+     */
+
+    shared_ptr<Node_> operator+(const Number_& lhs, const Number_& rhs) {
+        return shared_ptr<Node_>(new PlusNode_(lhs.Node(), rhs.Node()));
+    }
+
+    shared_ptr<Node_> operator*(const Number_& lhs, const Number_& rhs) {
+        return shared_ptr<Node_>(new TimesNode_(lhs.Node(), rhs.Node()));
+    }
+
+    shared_ptr<Node_> Log(const Number_& arg) {
+        return shared_ptr<Node_>(new LogNode_(arg.Node()));
+    }
+}
--- dal/math/aad/toy1/aad.hpp
+++ dal/math/aad/toy1/aad.hpp
@@ -2,7 +2,123 @@
 // Created by wegam on 2020/12/2.
 //
 
-#ifndef DAL_AAD_HPP
-#define DAL_AAD_HPP
+#pragma once
 
-#endif //DAL_AAD_HPP
+#include <memory>
+#include <queue>
+#include <dal/platform/platform.hpp>
+#include <dal/math/vectors.hpp>
+
+namespace Dal {
+    using std::shared_ptr;
+    class Node_ {
+    protected:
+        Vector_<shared_ptr<Node_>> arguments_;
+        bool processed_ = false;
+        unsigned order_ = 0;
+        double result_;
+        double adjoint_ = 0.;
+
+    public:
+        virtual ~Node_() = default;
+
+        template <class V_>
+        void PostOrder(V_& visitFunc) {
+            if (!processed_) {
+                for (const auto& arg : arguments_)
+                    arg->PostOrder(visitFunc);
+                visitFunc(*this);
+                processed_ = true;
+            }
+        }
+
+        template <class V_>
+        void PreOrder(V_& visitFunc) {
+            visitFunc(*this);
+            for (const auto& arg : arguments_)
+                arg->PreOrder(visitFunc);
+        }
+
+        template <class V_>
+        void BreadthFirst(V_& visitFunc) {
+            std::queue<std::shared_ptr<Node_>> q;
+            BreadthFirst(visitFunc, q);
+        }
+
+        template <class V_>
+        void BreadthFirst(V_& visitFunc, std::queue<std::shared_ptr<Node_>>& q) {
+            for (const auto& arg : arguments_)
+                q.push(arg);
+
+            visitFunc(*this);
+            while (!q.empty()) {
+                auto n = q.front();
+                q.pop();
+                n->BreadthFirst(visitFunc, q);
+            }
+        }
+
+        double Result() const;
+        void ResetProcessed();
+        void SetOrder(unsigned order);
+        unsigned Order() const;
+        double& Adjoint();
+        void ResetAdjoints();
+
+        virtual void Evaluate() = 0;
+        virtual void LogInstruction() = 0;
+        virtual void PropagateAdjoint() = 0;
+    };
+
+    class PlusNode_ : public Node_ {
+    public:
+        PlusNode_(const shared_ptr<Node_>& lhs, const shared_ptr<Node_>& rhs);
+        void LogInstruction() override;
+        void PropagateAdjoint() override;
+        void Evaluate() override;
+    };
+
+    class TimesNode_ : public Node_ {
+    public:
+        TimesNode_(const shared_ptr<Node_>& lhs, const shared_ptr<Node_>& rhs);
+        void LogInstruction() override;
+        void PropagateAdjoint() override;
+        void Evaluate() override;
+    };
+
+    class LogNode_ : public Node_ {
+    public:
+        LogNode_(const shared_ptr<Node_>& arg);
+        void LogInstruction() override;
+        void PropagateAdjoint() override;
+        void Evaluate() override;
+    };
+    
+    class Leaf_ : public Node_ {
+        double value_;
+    public:
+        Leaf_(double val);
+        double GetVal() const;
+        void SetVal(double val);
+        void LogInstruction() override;
+        void PropagateAdjoint() override;
+        void Evaluate() override;
+    };
+
+    class Number_ {
+        shared_ptr<Node_> node_;
+    public:
+        Number_(double val);
+        Number_(const shared_ptr<Node_>& node);
+        shared_ptr<Node_> Node() const;
+        void SetVal(double val);
+        double GetVal() const;
+
+        double Evaluate();
+        void SetOrder();
+        void LogResult() const;
+        void LogProgram();
+        double& Adjoint();
+        void PropagateAdjoints();
+    };
+}
