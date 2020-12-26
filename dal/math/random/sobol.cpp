943c3fc0c5698585153ec9bf192e0301f3f52ff0
wegamekinglc <scrappedprince.li@gmail.com>
2020-12-26 16:17:27 +0800

    FEATURE: remove comment and some codes change

--- dal/math/random/sobol.cpp
+++ dal/math/random/sobol.cpp
@@ -13,14 +13,11 @@ namespace Dal {
     namespace {
         constexpr int N_BITS = 30;
         constexpr auto XOR = [](int i, int j) -> int { return i ^ j; };
-
-        // precomputed primitive polynomials
         constexpr unsigned int KNOWN_PRIMITIVE[] = {0x0000002, 0x0000003, 0x00000005, 0x0000000A};
         constexpr int N_KNOWN = sizeof(KNOWN_PRIMITIVE) / sizeof(KNOWN_PRIMITIVE[0]);
 
         Matrix_<int> Directions(int size) {
             REQUIRE(size < N_KNOWN, "Not enough primitive polynomials available to generate Sobol sequences");
-            // body is left as an exercise to the reader, unless the author gets here first
             return Matrix_<int>(N_BITS, size);
         }
 
@@ -30,7 +27,7 @@ namespace Dal {
         }
 
         struct SobolSet_ : QuasiRandom::SequenceSet_ {
-            Matrix_<int> directions_; // index as [i_bit][i_seq]
+            Matrix_<int> directions_;
             int iPath_;
             Vector_<int> state_;
 
@@ -45,9 +42,9 @@ namespace Dal {
         };
 
         void SobolSet_::Next(Vector_<>* dst) {
-            dst->Resize(Size()); // usually no-op
+            dst->Resize(Size());
             ++iPath_;
-            REQUIRE(iPath_ != 0, "Next loop may not terminate"); // so next loop can terminate
+            REQUIRE(iPath_ != 0, "Next loop may not terminate");
             int k = 0;
             for (int j = iPath_; !(j & 1); j >>= 1, ++k)
                 ;
@@ -58,36 +55,33 @@ namespace Dal {
 
         SobolSet_* SobolSet_::TakeAway(int sub_size) {
             REQUIRE(sub_size > 0 && sub_size <= Size(), "Invalid sequence sub_size");
-            std::unique_ptr<SobolSet_> retval(new SobolSet_(iPath_));
+            std::unique_ptr<SobolSet_> ret_val(new SobolSet_(iPath_));
             if (sub_size == Size()) {
-                // give away all our contents
-                directions_.Swap(&retval->directions_);
-                state_.Swap(&retval->state_);
+                directions_.Swap(&ret_val->directions_);
+                state_.Swap(&ret_val->state_);
             } else {
-                // give it the back of our state/direction
-                const int mySize = Size() - sub_size;
-                Matrix_<int> myDir(N_BITS, mySize);
-                retval->directions_.Resize(N_BITS, sub_size);
+                const int size = Size() - sub_size;
+                Matrix_<int> dir(N_BITS, size);
+                ret_val->directions_.Resize(N_BITS, sub_size);
                 for (int ii = 0; ii < N_BITS; ++ii) {
-                    copy(directions_.Row(ii).begin(), directions_.Row(ii).begin() + mySize, myDir.Row(ii).begin());
-                    copy(directions_.Row(ii).begin() + mySize, directions_.Row(ii).end(),
-                         retval->directions_.Row(ii).begin());
+                    copy(directions_.Row(ii).begin(), directions_.Row(ii).begin() + size, dir.Row(ii).begin());
+                    copy(directions_.Row(ii).begin() + size, directions_.Row(ii).end(),
+                         ret_val->directions_.Row(ii).begin());
                 }
-                directions_.Swap(&myDir);
-                retval->state_.Assign(state_.begin() + mySize, state_.end());
-                state_.Resize(mySize);
+                directions_.Swap(&dir);
+                ret_val->state_.Assign(state_.begin() + size, state_.end());
+                state_.Resize(size);
             }
-            return retval.release();
+            return ret_val.release();
         }
     } // namespace
 
     QuasiRandom::SequenceSet_* QuasiRandom::NewSobol(int size, int i_path) {
         std::unique_ptr<SobolSet_> seq(new SobolSet_(i_path));
         seq->directions_ = Directions(size);
-        // initialize state from directions
         Fill(&seq->state_, 0);
         for (int jj = 0, ip = i_path; ip; ++jj, ip >>= 1) {
-            if ((ip ^ (ip >> 1)) & 1) // the Gray code of iPath_ has a 1<<jj bit
+            if ((ip ^ (ip >> 1)) & 1)
                 Transform(&seq->state_, seq->directions_.Row(jj), XOR);
         }
         return seq.release();
