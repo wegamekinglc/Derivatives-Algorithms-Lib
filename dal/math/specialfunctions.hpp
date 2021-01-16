0371a1933ab04569833a9e36e26be9e204cf80fb
wegamekinglc <scrappedprince.li@gmail.com>
2021-01-16 14:24:58 +0800

    FEATURE: added NPDF

--- dal/math/specialfunctions.hpp
+++ dal/math/specialfunctions.hpp
@@ -4,7 +4,12 @@
 
 #pragma once
 
+#include <cmath>
+
 namespace Dal {
+    inline double NPDF(double z) {
+        return z < -10.0 || 10.0 < z ? 0.0 : std::exp(-0.5 * z * z) / 2.506628274631;
+    }
     double NCDF(double z, bool precise = true);
     double InverseNCDF(double x, bool precise = true, bool polish = true);
-}
+} // namespace Dal
