f429b706020701348b0d38b07166f8b4bc7043ec
wegamekinglc <scrappedprince.li@gmail.com>
2021-01-21 15:05:04 +0800

    TEST: added number's add test

---
+++ tests/math/aad/number.cpp
@@ -0,0 +1,35 @@
+//
+// Created by wegam on 2021/1/18.
+//
+
+#undef AADET_ENABLED
+
+#include <dal/math/aad/aad.hpp>
+#include <gtest/gtest.h>
+
+using namespace Dal;
+
+TEST(AADNumberTest, TestNumberAdd) {
+    Number_ s1(1.0);
+    Number_ s2(2.0);
+
+    auto value = s1 + s2;
+    ASSERT_NEAR(value.Value(), 3.0, 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    ASSERT_NEAR(s2.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s1 = 1.0;
+    value = s1 + 2.0;
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s2 = 2.0;
+    value = 1.0 + s2;
+    value.PropagateToStart();
+    ASSERT_NEAR(s2.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+}
+
