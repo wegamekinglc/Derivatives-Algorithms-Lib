aec6f7d24f98a5258dab59c8fca5388e40763e70
wegamekinglc <scrappedprince.li@gmail.com>
2021-01-22 16:54:28 +0800

    TEST: added tests for pow and max

--- tests/math/aad/number.cpp
+++ tests/math/aad/number.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <dal/math/aad/aad.hpp>
+#include <dal/math/aad/number.hpp>
+#include <cmath>
 #include <gtest/gtest.h>
 
 using namespace Dal;
@@ -115,3 +117,70 @@ TEST(AADNumberTest, TestNumberDivide) {
     Number_::tape_->Rewind();
 }
 
+TEST(AADNumberTest, TestNumberPow) {
+    Number_::tape_->Clear();
+
+    Number_ s1(3.0);
+    Number_ s2(2.0);
+    auto value = Pow(s1, s2);
+    ASSERT_NEAR(value.Value(), std::pow(s1.Value(), s2.Value()), 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), s2.Value() * std::pow(s1.Value(), s2.Value() - 1), 1e-10);
+    ASSERT_NEAR(s2.Adjoint(), value.Value() * std::log(s1.Value()), 1e-10);
+
+    s1 = 3.0;
+    value = Pow(s1, 2.0);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), s2.Value() * std::pow(s1.Value(), 1.0), 1e-10);
+    Number_::tape_->Rewind();
+
+    s2 = 2.0;
+    value = Pow(3.0, s2);
+    value.PropagateToStart();
+    ASSERT_NEAR(s2.Adjoint(), value.Value() * std::log(3.0), 1e-10);
+    Number_::tape_->Rewind();
+}
+
+TEST(AADNumberTest, TestNumberMax) {
+    Number_::tape_->Clear();
+
+    Number_ s1(3.0);
+    Number_ s2(2.0);
+    auto value = Max(s1, s2);
+    ASSERT_NEAR(value.Value(), 3.0, 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    ASSERT_NEAR(s2.Adjoint(), 0.0, 1e-10);
+
+    s1 = 3.0;
+    value = Max(s1, 2.0);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s2 = 2.0;
+    value = Max(3.0, s2);
+    value.PropagateToStart();
+    ASSERT_NEAR(s2.Adjoint(), 0.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s1 = 2.0;
+    s2 = 3.0;
+    value = Max(s1, s2);
+    ASSERT_NEAR(value.Value(), 3.0, 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 0.0, 1e-10);
+    ASSERT_NEAR(s2.Adjoint(), 1.0, 1e-10);
+
+    s1 = 2.0;
+    value = Max(s1, 3.0);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 0.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s2 = 3.0;
+    value = Max(2.0, s2);
+    value.PropagateToStart();
+    ASSERT_NEAR(s2.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+}
