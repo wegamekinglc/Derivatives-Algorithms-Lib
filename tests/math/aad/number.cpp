8ab0c56a91cdb57c1a8afa9eb8bfa627f773ea1c
wegamekinglc <scrappedprince.li@gmail.com>
2021-01-22 14:29:39 +0800

    TEST: added tests for sub, multiply and divide.

--- tests/math/aad/number.cpp
+++ tests/math/aad/number.cpp
@@ -34,3 +34,84 @@ TEST(AADNumberTest, TestNumberAdd) {
     Number_::tape_->Rewind();
 }
 
+TEST(AADNumberTest, TestNumberSub) {
+
+    Number_::tape_->Clear();
+
+    Number_ s1(1.0);
+    Number_ s2(2.0);
+
+    auto value = s1 - s2;
+    ASSERT_NEAR(value.Value(), -1.0, 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    ASSERT_NEAR(s2.Adjoint(), -1.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s1 = 1.0;
+    value = s1 - 2.0;
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s2 = 2.0;
+    value = 1.0 - s2;
+    value.PropagateToStart();
+    ASSERT_NEAR(s2.Adjoint(), -1.0, 1e-10);
+    Number_::tape_->Rewind();
+}
+
+TEST(AADNumberTest, TestNumberMultiply) {
+
+    Number_::tape_->Clear();
+
+    Number_ s1(3.0);
+    Number_ s2(2.0);
+
+    auto value = s1 * s2;
+    ASSERT_NEAR(value.Value(), 6.0, 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 2.0, 1e-10);
+    ASSERT_NEAR(s2.Adjoint(), 3.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s1 = 3.0;
+    value = s1 * 2.0;
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 2.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s2 = 2.0;
+    value = 3.0 * s2;
+    value.PropagateToStart();
+    ASSERT_NEAR(s2.Adjoint(), 3.0, 1e-10);
+    Number_::tape_->Rewind();
+}
+
+TEST(AADNumberTest, TestNumberDivide) {
+
+    Number_::tape_->Clear();
+
+    Number_ s1(3.0);
+    Number_ s2(2.0);
+
+    auto value = s1 / s2;
+    ASSERT_NEAR(value.Value(), 1.5, 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1./ s2.Value(), 1e-10);
+    ASSERT_NEAR(s2.Adjoint(), -s1.Value() / s2.Value() / s2.Value(), 1e-10);
+    Number_::tape_->Rewind();
+
+    s1 = 3.0;
+    value = s1 / 2.0;
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1. / 2.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s2 = 2.0;
+    value = 3.0 / s2;
+    value.PropagateToStart();
+    ASSERT_NEAR(s2.Adjoint(), -3.0 / s2.Value() / s2.Value(), 1e-10);
+    Number_::tape_->Rewind();
+}
+
