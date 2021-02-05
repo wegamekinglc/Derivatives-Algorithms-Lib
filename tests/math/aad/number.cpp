851f8558ee3af831b0a78e8ff15651750e6b9f41
wegamekinglc <scrappedprince.li@gmail.com>
2021-02-05 17:20:19 +0800

    TEST: added many tests

--- tests/math/aad/number.cpp
+++ tests/math/aad/number.cpp
@@ -2,8 +2,9 @@
 // Created by wegam on 2021/1/18.
 //
 
+#ifndef AADET_ENABLED
+
 #include <dal/math/aad/aad.hpp>
-#include <dal/math/aad/number.hpp>
 #include <cmath>
 #include <gtest/gtest.h>
 
@@ -228,3 +229,169 @@ TEST(AADNumberTest, TestNumberMin) {
     ASSERT_NEAR(s2.Adjoint(), 0.0, 1e-10);
     Number_::tape_->Rewind();
 }
+
+
+TEST(AADNumberTest, TestNumberEqualAdd) {
+
+    Number_::tape_->Clear();
+
+    Number_ s1(1.0);
+    Number_ s2(2.0);
+
+    s1 += s2;
+    ASSERT_NEAR(s1.Value(), 3.0, 1e-10);
+    s1.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    ASSERT_NEAR(s2.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s1 = 1.0;
+    s1 += 2.0;
+    ASSERT_NEAR(s1.Value(), 3.0, 1e-10);
+    s1.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+}
+
+
+TEST(AADNumberTest, TestNumberEqualSub) {
+
+    Number_::tape_->Clear();
+
+    Number_ s1(1.0);
+    Number_ s2(2.0);
+
+    s1 -= s2;
+    ASSERT_NEAR(s1.Value(), -1.0, 1e-10);
+    s1.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    ASSERT_NEAR(s2.Adjoint(), -1.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s1 = 1.0;
+    s1 -= 2.0;
+    ASSERT_NEAR(s1.Value(), -1.0, 1e-10);
+    s1.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+}
+
+
+TEST(AADNumberTest, TestNumberEqualMultiply) {
+
+    Number_::tape_->Clear();
+
+    Number_ s1(2.0);
+    Number_ s2(3.0);
+
+    s1 *= s2;
+    ASSERT_NEAR(s1.Value(), 6.0, 1e-10);
+    s1.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    ASSERT_NEAR(s2.Adjoint(), 2.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s1 = 2.0;
+    s1 *= 3.0;
+    ASSERT_NEAR(s1.Value(), 6.0, 1e-10);
+    s1.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+}
+
+
+TEST(AADNumberTest, TestNumberEqualDivide) {
+
+    Number_::tape_->Clear();
+
+    Number_ s1(2.0);
+    Number_ s2(3.0);
+
+    s1 /= s2;
+    ASSERT_NEAR(s1.Value(), 0.66666666666666, 1e-10);
+    s1.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    ASSERT_NEAR(s2.Adjoint(), -2.0 / 9.0, 1e-10);
+    Number_::tape_->Rewind();
+
+    s1 = 2.0;
+    s1 /= 3.0;
+    ASSERT_NEAR(s1.Value(), 0.66666666666666, 1e-10);
+    s1.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+}
+
+
+TEST(AADNumberTest, TestNumberNegative) {
+    Number_::tape_->Clear();
+    Number_ s1(2.0);
+
+    auto value = -s1;
+    ASSERT_NEAR(value.Value(), -2.0, 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), -1.0, 1e-10);
+    Number_::tape_->Rewind();
+}
+
+
+TEST(AADNumberTest, TestNumberPositive) {
+    Number_::tape_->Clear();
+    Number_ s1(2.0);
+
+    auto value = +s1;
+    ASSERT_NEAR(value.Value(), 2.0, 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 1.0, 1e-10);
+    Number_::tape_->Rewind();
+}
+
+
+TEST(AADNumberTest, TestNumberExp) {
+    Number_::tape_->Clear();
+    Number_ s1(2.0);
+
+    auto value = Exp(s1);
+    ASSERT_NEAR(value.Value(), std::exp(2.0), 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), std::exp(2.0), 1e-10);
+    Number_::tape_->Rewind();
+}
+
+
+TEST(AADNumberTest, TestNumberLog) {
+    Number_::tape_->Clear();
+    Number_ s1(2.0);
+
+    auto value = Log(s1);
+    ASSERT_NEAR(value.Value(), std::log(2.0), 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 0.5, 1e-10);
+    Number_::tape_->Rewind();
+}
+
+
+TEST(AADNumberTest, TestNumberSqrt) {
+    Number_::tape_->Clear();
+    Number_ s1(2.0);
+
+    auto value = Sqrt(s1);
+    ASSERT_NEAR(value.Value(), std::sqrt(2.0), 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), 0.5 / std::sqrt(2.0), 1e-10);
+    Number_::tape_->Rewind();
+}
+
+
+TEST(AADNumberTest, TestNumberFabs) {
+    Number_::tape_->Clear();
+    Number_ s1(-2.0);
+
+    auto value = Fabs(s1);
+    ASSERT_NEAR(value.Value(), 2.0, 1e-10);
+    value.PropagateToStart();
+    ASSERT_NEAR(s1.Adjoint(), -1.0, 1e-10);
+    Number_::tape_->Rewind();
+}
+
+#endif
