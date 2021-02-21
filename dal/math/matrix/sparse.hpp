6f58523192937294c32b2dd51d66d3a34458a065
wegamekinglc <scrappedprince.li@gmail.com>
2021-02-22 02:49:20 +0800

    FEATURE: added sparse matrix

---
+++ dal/math/matrix/sparse.hpp
@@ -0,0 +1,33 @@
+//
+// Created by wegam on 2021/2/22.
+//
+
+#pragma once
+
+#include <dal/math/matrix/decompositions.hpp>
+
+namespace Dal::Sparse {
+
+    class SymmetricDecomposition_ : public SymmetricMatrixDecomposition_ {
+    public:
+        // form J^T A^{-1} J for given J
+        virtual void QForm(const Matrix_<>& j_mat, SquareMatrix_<>* dst) const;
+    };
+
+    class Square_ : noncopyable {
+    public:
+        virtual int Size() const = 0;
+        virtual void MultiplyLeft(const Vector_<>& x, Vector_<>* b) const = 0;
+        virtual void MultiplyRight(const Vector_<>& x, Vector_<>* b) const = 0;
+
+        virtual bool IsSymmetric() const = 0;
+        virtual SquareMatrixDecomposition_* Decompose() const = 0;
+        SymmetricDecomposition_* DecomposeSymmetric() const;
+        virtual const double& operator()(int i_row, int j_col) const = 0;
+        virtual void Set(int i_row, int j_col, double val) = 0;
+        virtual void Add(int i_row, int j_col, double val) {
+            Set(i_row, j_col, val + operator()(i_row, j_col));
+        }
+    };
+
+}
---
+++ dal/math/matrix/squarematrix.hpp
@@ -0,0 +1,39 @@
+//
+// Created by wegam on 2021/2/22.
+//
+
+#pragma once
+
+#include <dal/math/matrix/matrixs.hpp>
+
+namespace Dal {
+    template <class E_>
+    class SquareMatrix_ {
+        Matrix_<E_> val_;
+
+    public:
+        SquareMatrix_() = default;
+        SquareMatrix_(int size): val_(size, size) {}
+        void Resize(int size) { val_.Resize(size, size); }
+
+        operator const Matrix_<E_>&() const { return val_; };
+        double& operator()(int i_row, int j_col) { return val_(i_row, j_col); }
+        const double& operator()(int i_row, int j_col) const { return val_(i_row, j_col); }
+
+        int Rows() const { return val_.Rows(); }
+        typename Matrix_<E_>::Row_ Row(int ii) { return val_.Row(ii); }
+        typename Matrix_<E_>::ConstRow_ Row(int ii) const { return val_.Row(ii); }
+        int Cols() const { return val_.Cols(); }
+        typename Matrix_<E_>::Col_ Col(int ii) { return val_.Col(ii); }
+        typename Matrix_<E_>::ConstCol_ Col(int ii) const { return val_.Col(ii); }
+    };
+
+    namespace SquareMatrix {
+        template <class E_>
+        SquareMatrix_<E_> M1x1(const E_& val) {
+            SquareMatrix_<E_> ret_val(1);
+            ret_val(0, 0) = val;
+            return ret_val;
+        }
+    }
+}
