7ce5962a7c80268be5d8f15b01a2afe54054a380
wegamekinglc <scrappedprince.li@gmail.com>
2021-02-21 15:07:44 +0800

    FEATURE: added decompositions

---
+++ dal/math/matrix/decompositions.cpp
@@ -0,0 +1,21 @@
+//
+// Created by wegam on 2021/2/20.
+//
+
+#include <dal/math/matrix/decompositions.hpp>
+#include <dal/platform/strict.hpp>
+
+#define COPY_ALIAS_AND_FORWARD(cname, func, imp)    \
+void cname::func(const Vector_<>& x, Vector_<>* b) const {    \
+    if (&x == b)                                              \
+        imp(Vector_<>(x), b);                                 \
+    else                                                      \
+        imp(x, b);                                            \
+}
+
+namespace Dal {
+    COPY_ALIAS_AND_FORWARD(SquareMatrixDecomposition_, SolveLeft, XSolveLeft_af)
+    COPY_ALIAS_AND_FORWARD(SquareMatrixDecomposition_, SolveRight, XSolveRight_af)
+    COPY_ALIAS_AND_FORWARD(SymmetricMatrixDecomposition_, Multiply, XMultiply_af)
+    COPY_ALIAS_AND_FORWARD(SymmetricMatrixDecomposition_, Solve, XSolve_af)
+}
---
+++ dal/math/matrix/decompositions.hpp
@@ -0,0 +1,58 @@
+//
+// Created by wegam on 2021/2/19.
+//
+
+#pragma once
+
+#include <dal/platform/platform.hpp>
+#include <dal/math/vectors.hpp>
+#include <dal/utilities/noncopyable.hpp>
+
+namespace Dal {
+    class SquareMatrixDecomposition_ : noncopyable {
+        virtual void XMultiplyLeft_af(const Vector_<>& x, Vector_<>* b) const = 0;
+        virtual void XMultiplyRight_af(const Vector_<>& x, Vector_<>* b) const = 0;
+        virtual void XSolveLeft_af(const Vector_<>& b, Vector_<>* x) const = 0;
+        virtual void XSolveRight_af(const Vector_<>& b, Vector_<>* x) const = 0;
+
+    public:
+        virtual ~SquareMatrixDecomposition_() = default;
+        virtual int Size() const = 0;
+        void MultiplyLeft(const Vector_<>& x, Vector_<>* b) const;
+        void MultiplyRight(const Vector_<>& x, Vector_<>* b) const;
+        void SolveLeft(const Vector_<>& b, Vector_<>* x) const;
+        void SolveRight(const Vector_<>& b, Vector_<>* x) const;
+    };
+
+    // special case of symmetric matrix
+
+    class SymmetricMatrixDecomposition_ : public SquareMatrixDecomposition_ {
+        virtual void XMultiply_af(const Vector_<>& x, Vector_<>* b) const = 0;
+
+        void XMultiplyLeft_af(const Vector_<>& x, Vector_<>* b) const override {
+            return XMultiply_af(x, b);
+        }
+
+        void XMultiplyRight_af(const Vector_<>& x, Vector_<>* b) const override {
+            return XMultiply_af(x, b);
+        }
+
+        virtual void XSolve_af(const Vector_<>& b, Vector_<>* x) const = 0;
+
+        void XSolveLeft_af(const Vector_<>& b, Vector_<>* x) const override {
+            return XSolve_af(b, x);
+        }
+
+        void XSolveRight_af(const Vector_<>& b, Vector_<>* x) const override {
+            return XSolve_af(b, x);
+        }
+
+    public:
+        virtual int Rank() const { return Size(); }
+        virtual Vector_<>::const_iterator MakeCorrelated(Vector_<>::const_iterator iid_begin,
+                                                         Vector_<>* correlated) const = 0;
+
+        void Multiply(const Vector_<>& x, Vector_<>* b) const;
+        void Solve(const Vector_<>& b, Vector_<>* x) const;
+    };
+}
