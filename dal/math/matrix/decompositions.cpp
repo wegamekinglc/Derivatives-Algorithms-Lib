a8e33fe0b9859e3ca68c5a647394adcd2df48ab3
wegamekinglc <scrappedprince.li@gmail.com>
2021-04-16 22:37:23 +0800

    REFACTOR: renaming tests files

--- dal/math/matrix/decompositions.cpp
+++ dal/math/matrix/decompositions.cpp
@@ -16,6 +16,8 @@ void cname::func(const Vector_<>& x, Vector_<>* b) const {    \
 namespace Dal {
     COPY_ALIAS_AND_FORWARD(SquareMatrixDecomposition_, SolveLeft, XSolveLeft_af)
     COPY_ALIAS_AND_FORWARD(SquareMatrixDecomposition_, SolveRight, XSolveRight_af)
-    COPY_ALIAS_AND_FORWARD(SymmetricMatrixDecomposition_, Multiply, XMultiply_af)
+    COPY_ALIAS_AND_FORWARD(SquareMatrixDecomposition_, MultiplyLeft, XMultiplyLeft_af)
+    COPY_ALIAS_AND_FORWARD(SquareMatrixDecomposition_, MultiplyRight, XMultiplyRight_af)
     COPY_ALIAS_AND_FORWARD(SymmetricMatrixDecomposition_, Solve, XSolve_af)
+    COPY_ALIAS_AND_FORWARD(SymmetricMatrixDecomposition_, Multiply, XMultiply_af)
 }
