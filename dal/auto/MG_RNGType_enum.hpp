15064d886c56c06c4168250e339e4b5b7a8a7b7a
wegamekinglc <scrappedprince.li@gmail.com>
2021-09-05 21:54:38 +0800

    FIX: added more auto codes

---
+++ dal/auto/MG_RNGType_enum.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+class  RNGType_
+{
+    enum class Value_ : char
+    {
+     _NOT_SET=-1,
+     IRN,
+     MRG32,
+     _N_VALUES
+    } val_;
+      
+    RNGType_(Value_ val) : val_(val) {
+        REQUIRE(val < Value_::_N_VALUES, "val is not valid");
+    }
+    friend bool operator==(const RNGType_& lhs, const RNGType_& rhs);
+    friend struct ReadStringRNGType_;
+    friend Vector_<RNGType_> RNGTypeListAll();
+    friend bool operator<(const RNGType_& lhs, const RNGType_& rhs) {
+        return lhs.val_ < rhs.val_;
+    }
+public:
+    explicit RNGType_(const String_& src);
+    const char* String() const;
+    RNGType_() : val_(Value_::_NOT_SET) {};
+};
+
+Vector_<RNGType_> RNGTypeListAll();
+
+bool operator==(const RNGType_& lhs, const RNGType_& rhs);
+inline bool operator!=(const RNGType_& lhs, const RNGType_& rhs) {return !(lhs == rhs);}
---
+++ dal/protocol/currency.hpp
@@ -0,0 +1,24 @@
+//
+// Created by wegam on 2021/9/5.
+//
+
+#pragma once
+
+#include <dal/string/strings.hpp>
+
+namespace Dal {
+/*IF--------------------------------------------------------------------------
+enumeration Ccy
+        A currency
+switchable
+alternative USD
+alternative EUR
+alternative GBP
+alternative JPY
+alternative AUD
+alternative CHF
+alternative CAD
+-IF-------------------------------------------------------------------------*/
+
+#include <dal/auto/MG_Ccy_enum.hpp>
+}
