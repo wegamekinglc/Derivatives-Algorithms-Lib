e04584eae6952cbcb13b604edd35c9b573440abf
wegamekinglc <scrappedprince.li@gmail.com>
2022-01-22 01:31:05 +0800

    FEATURE: added tests for equity index name

--- dal/indice/indexequity.hpp
+++ dal/indice/indexequity.hpp
@@ -10,9 +10,9 @@
 namespace Dal::Index {
     class Equity_: public Index_ {
         Cell_ delivery_;
-        String_ Name() const;
 
     public:
+        String_ Name() const;
         const String_ eqName_;
         Date_ Delivery(const DateTime_& fixing_time) const;
         Equity_(const String_& eq_name, const Date_* delivery_date = nullptr, const String_* delay_increment = nullptr);
---
+++ tests/indice/test_indexequity.cpp
@@ -0,0 +1,18 @@
+//
+// Created by wegam on 2022/1/22.
+//
+
+#include <gtest/gtest.h>
+#include <dal/indice/indexequity.hpp>
+
+using namespace Dal;
+
+TEST(IndexEquityTest, TestEquityName) {
+    String_ delay("3M");
+    Index::Equity_ eq("HS300", nullptr, &delay);
+    ASSERT_EQ(String_("EQ[HS300]>3M"), eq.Name());
+
+    Date_ date(2022, 1, 22);
+    Index::Equity_ eq2("HS300", &date);
+    ASSERT_EQ(String_("EQ[HS300]@2022-01-22"), eq2.Name());
+}
