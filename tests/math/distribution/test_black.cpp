8fca635f5e3b59a814a764187ad807f89c3b452b
wegamekinglc <scrappedprince.li@gmail.com>
2022-05-07 21:32:04 +0800

    added first tests for black

--- tests/math/distribution/test_black.cpp
+++ tests/math/distribution/test_black.cpp
@@ -1,3 +1,25 @@
 //
 // Created by wegam on 2022/5/7.
 //
+
+#include <gtest/gtest.h>
+#include <dal/math/distribution/black.hpp>
+
+using namespace Dal;
+
+TEST(DistributionBlackTest, TestBlackOptionPrice) {
+    const auto vol = 0.2;
+    const auto T = 2.0;
+    const auto forward = 110.0;
+    const auto dean_vol = vol * sqrt(T);
+    DistributionBlack_ black(forward, dean_vol);
+
+    auto call_price = black.OptionPrice(120.0, OptionType_("Call"));
+    ASSERT_NEAR(call_price, 8.53592506, 1e-6);
+
+    auto put_price = black.OptionPrice(120.0, OptionType_("Put"));
+    ASSERT_NEAR(put_price, 18.53592506, 1e-6);
+
+    auto straddle_price = black.OptionPrice(120.0, OptionType_("Straddle"));
+    ASSERT_NEAR(straddle_price, 27.07185013, 1e-6);
+}
