14dd5b0caca9d670dcab61e973f9d7712e01e072
wegamekinglc <scrappedprince.li@gmail.com>
2022-05-08 10:52:03 +0800

    using std optional

--- dal/platform/optionals.hpp
+++ dal/platform/optionals.hpp
@@ -4,14 +4,14 @@
 
 #pragma once
 
-#include <boost/optional.hpp>
+#include <optional>
 
 // syntactic sugar for null default
-template <class T_> const T_& operator+(const boost::optional<T_>& opt) { return opt.get_value_or(T_()); }
+template <class T_> const T_& operator+(const std::optional<T_>& opt) { return opt.get_value_or(T_()); }
 
 // construct from a pointer
-template <class T_> boost::optional<T_> as_optional(const T_* p) {
-    boost::optional<T_> ret_val;
+template <class T_> std::optional<T_> as_optional(const T_* p) {
+    std::optional<T_> ret_val;
     if (p)
         ret_val = *p;
     return ret_val;
---
+++ public/_jni.hpp
@@ -0,0 +1,8 @@
+//
+// Created by wegam on 2022/5/8.
+//
+
+#ifndef DAL__JNI_HPP
+#define DAL__JNI_HPP
+
+#endif //DAL__JNI_HPP
