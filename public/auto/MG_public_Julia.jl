53d2535711314936fa486f8f39352ed85b0425ca
wegamekinglc <scrappedprince.li@gmail.com>
2022-05-09 21:01:50 +0800

    fixed error

---
+++ public/auto/MG_public_Julia.jl
@@ -0,0 +1,13 @@
+
+include("MG_Repository_Erase.jl")
+include("MG_Repository_Find.jl")
+include("MG_Repository_Size.jl")
+include("MG_Interp1_New_Linear.jl")
+include("MG_Interp1_New_Linear_Smoothed.jl")
+include("MG_Interp1_New_Cubic.jl")
+include("MG_Interp1_Get.jl")
+include("MG_Interp2_New_Linear.jl")
+include("MG_Interp2_Get.jl")
+include("MG_Format.jl")
+include("MG_PasteWithArgs.jl")
+
