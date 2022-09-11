df5f20377ee60e8729cf0dbec570372783bfb447
wegamekinglc <scrappedprince.li@gmail.com>
2022-09-11 21:54:32 +0800

    remove unused function

--- dal/math/aad/models/gaussians.hpp
+++ dal/math/aad/models/gaussians.hpp
@@ -38,45 +38,4 @@ namespace Dal::AAD {
 
         return 1.0 - pdf * pol;
     }
-
-    inline double InvNormalCDF(double p) {
-        const bool sup = p > 0.5;
-        const double up = sup ? 1.0 - p : p;
-
-        static constexpr double a0 = 2.50662823884;
-        static constexpr double a1 = -18.61500062529;
-        static constexpr double a2 = 41.39119773534;
-        static constexpr double a3 = -25.44106049637;
-
-        static constexpr double b0 = -8.47351093090;
-        static constexpr double b1 = 23.08336743743;
-        static constexpr double b2 = -21.06224101826;
-        static constexpr double b3 = 3.13082909833;
-
-        static constexpr double c0 = 0.3374754822726147;
-        static constexpr double c1 = 0.9761690190917186;
-        static constexpr double c2 = 0.1607979714918209;
-        static constexpr double c3 = 0.0276438810333863;
-        static constexpr double c4 = 0.0038405729373609;
-        static constexpr double c5 = 0.0003951896511919;
-        static constexpr double c6 = 0.0000321767881768;
-        static constexpr double c7 = 0.0000002888167364;
-        static constexpr double c8 = 0.0000003960315187;
-
-        double x = up - 0.5;
-        double r;
-
-        if (Fabs(x) < 0.42) {
-            r = x * x;
-            r = x * (((a3 * r + a2) * r + a1) * r + a0) / ((((b3 * r + b2) * r + b1) * r + b0) * r + 1.0);
-            return sup ? -r : r;
-        }
-
-        r = up;
-        r = Log(-Log(r));
-        r = c0 + r * (c1 + r * (c2 + r * (c3 + r * (c4 + r * (c5 + r * (c6 + r * (c7 + r * c8)))))));
-
-        return sup ? r : -r;
-    }
-
 } // namespace Dal::AAD
