8b7ef841859927071df8af7e908f83192ab01700
wegamekinglc <scrappedprince.li@gmail.com>
2022-10-06 13:53:04 +0800

    remove static cast

--- dal/math/aad/models/dupire.hpp
+++ dal/math/aad/models/dupire.hpp
@@ -92,7 +92,7 @@ namespace Dal::AAD {
                 const size_t m = logSpots_.size();
                 for (size_t j = 0; j < m; ++j)
                     interpVols_[i][j] =
-                        sqrtDt * InterpLinearImplX(times_, static_cast<Vector_<T_>>(vols_.Col(j)), timeLine_[i]);
+                        sqrtDt * InterpLinearImplX<false, T_>(times_, vols_.Col(j), timeLine_[i]);
             }
         }
 
@@ -108,9 +108,8 @@ namespace Dal::AAD {
 
             //  Iterate through timeline
             const size_t n = timeLine_.size() - 1;
-            const size_t m = logSpots_.size();
             for (size_t i = 0; i < n; ++i) {
-                T_ vol = InterpLinearImplX(logSpots_, static_cast<Vector_<T_>>(interpVols_.Row(i)), logSpot);
+                T_ vol = InterpLinearImplX<false, T_>(logSpots_, interpVols_.Row(i), logSpot);
                 logSpot += vol * (-0.5 * vol + gaussVec[i]);
                 if (commonSteps_[i + 1]) {
                     FillScenario(Exp(logSpot), (*path)[idx]);
