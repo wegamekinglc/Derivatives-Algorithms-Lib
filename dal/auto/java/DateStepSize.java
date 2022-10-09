1bb1612daccb00458ecf1efe2ca69ce814a37972
wegamekinglc <scrappedprince.li@gmail.com>
2022-10-09 22:15:34 +0800

    added back files

---
+++ dal/auto/java/DateStepSize.java
@@ -0,0 +1,15 @@
+
+package types;
+
+public class DateStepSize
+{
+    public enum Value
+    {
+		Y,
+		M,
+		W,
+		BD,
+		CD,
+        N_VALUES
+    }
+}
---
+++ dal/auto/java/SpecialDay.java
@@ -0,0 +1,14 @@
+
+package types;
+
+public class SpecialDay
+{
+    public enum Value
+    {
+		IMM,
+		IMM1,
+		CDS,
+		EOM,
+        N_VALUES
+    }
+}
