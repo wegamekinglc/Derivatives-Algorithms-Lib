9f29e3523e0d439eca714f6c9a1408ceda2a3cd6
wegamekinglc <scrappedprince.li@gmail.com>
2022-11-12 22:42:09 +0800

    using experimental

--- dal/script/event.cpp
+++ dal/script/event.cpp
@@ -8,12 +8,8 @@
 
 
 namespace Dal::Script {
-    void ScriptProduct_::Visit(Visitor_& v) {
-        for (auto &evt: events_) {
-            for (auto &stat: evt)
-                v.VisitTree(stat);
-        }
-    }
+
+    using namespace Dal::Script::Experimental;
 
     void ScriptProduct_::IndexVariables() {
         VarIndexer_ indexer;
@@ -21,36 +17,36 @@ namespace Dal::Script {
         variables_ = indexer.GetVarNames();
     }
 
-    size_t ScriptProduct_::IFProcess() {
-        IFProcessor_ ifProc;
-        Visit(ifProc);
-        return ifProc.MaxNestedIFs();
-    }
-
-    //	Domain processing
-    void ScriptProduct_::DomainProcess(bool fuzzy) {
-        DomainProcessor_ domProc(variables_.size(), fuzzy);
-        Visit(domProc);
-    }
-
-    void ScriptProduct_::ConstCondProcess() {
-        ConstCondProcessor_ ccProc;
-        for (auto &evt: events_) {
-            for (auto &stat: evt)
-                ccProc.ProcessFromTop(stat);
-        }
-    }
+//    size_t ScriptProduct_::IFProcess() {
+//        IFProcessor_ ifProc;
+//        Visit(ifProc);
+//        return ifProc.MaxNestedIFs();
+//    }
+//
+//    //	Domain processing
+//    void ScriptProduct_::DomainProcess(bool fuzzy) {
+//        DomainProcessor_ domProc(variables_.size(), fuzzy);
+//        Visit(domProc);
+//    }
+//
+//    void ScriptProduct_::ConstCondProcess() {
+//        ConstCondProcessor_ ccProc;
+//        for (auto &evt: events_) {
+//            for (auto &stat: evt)
+//                ccProc.ProcessFromTop(stat);
+//        }
+//    }
 
     //	All preprocessing
     size_t ScriptProduct_::PreProcess(bool fuzzy, bool skipDoms) {
         IndexVariables();
 
         size_t maxNestedIfs = 0;
-        if (fuzzy || !skipDoms) {
-            maxNestedIfs = IFProcess();
-            DomainProcess(fuzzy);
-            ConstCondProcess();
-        }
+//        if (fuzzy || !skipDoms) {
+//            maxNestedIfs = IFProcess();
+//            DomainProcess(fuzzy);
+//            ConstCondProcess();
+//        }
 
         // generate time line and definition
         // TODO: more specific data settings
@@ -68,21 +64,21 @@ namespace Dal::Script {
     }
 
     //	Debug whole product
-    void ScriptProduct_::Debug(std::ostream &ost) {
-        size_t v = 0;
-        for (auto& variable: variables_)
-            ost << "Var[" << v++ << "] = " << variable << std::endl;
-
-        Debugger_ d;
-        size_t e = 0;
-        for (auto& evtIt: events_) {
-            ost << "Event: " << ++e << std::endl;
-            unsigned s = 0;
-            for (const auto &stat: evtIt) {
-                d.VisitTree(stat);
-                ost << "Statement: " << ++s << std::endl;
-                ost << d.String() << std::endl;
-            }
-        }
-    }
+//    void ScriptProduct_::Debug(std::ostream &ost) {
+//        size_t v = 0;
+//        for (auto& variable: variables_)
+//            ost << "Var[" << v++ << "] = " << variable << std::endl;
+//
+//        Debugger_ d;
+//        size_t e = 0;
+//        for (auto& evtIt: events_) {
+//            ost << "Event: " << ++e << std::endl;
+//            unsigned s = 0;
+//            for (const auto &stat: evtIt) {
+//                d.VisitTree(stat);
+//                ost << "Statement: " << ++s << std::endl;
+//                ost << d.String() << std::endl;
+//            }
+//        }
+//    }
 }
--- dal/script/event.hpp
+++ dal/script/event.hpp
@@ -6,21 +6,23 @@
 
 #include <dal/math/vectors.hpp>
 #include <dal/time/date.hpp>
-#include <dal/script/node.hpp>
-#include <dal/script/visitor/evaluator.hpp>
-#include <dal/script/visitor/fuzzy.hpp>
-#include <dal/script/visitor/varindexer.hpp>
-#include <dal/script/visitor/ifprocessor.hpp>
-#include <dal/script/visitor/domainproc.hpp>
-#include <dal/script/visitor/constcondprocessor.hpp>
-#include <dal/script/visitor/debugger.hpp>
+#include <dal/script/experimental/node.hpp>
+#include <dal/script/experimental/visitor/evaluator.hpp>
+//#include <dal/script/visitor/fuzzy.hpp>
+#include <dal/script/experimental/visitor/varindexer.hpp>
+//#include <dal/script/visitor/ifprocessor.hpp>
+//#include <dal/script/visitor/domainproc.hpp>
+//#include <dal/script/visitor/constcondprocessor.hpp>
+#include <dal/script/experimental/visitor/debugger.hpp>
 #include <dal/script/parser.hpp>
 #include <dal/math/aad/sample.hpp>
 
 
 namespace Dal::Script {
     using Dal::AAD::Scenario_;
-    using Event_ = Vector_<std::unique_ptr<ScriptNode_>>;
+    using namespace Dal::Script::Experimental;
+
+    using Event_ = Vector_<ScriptNode_>;
 
     class ScriptProduct_ {
         Vector_<Date_> eventDates_;
@@ -41,10 +43,10 @@ namespace Dal::Script {
             return std::unique_ptr<Evaluator_<T_>>(new Evaluator_<T_>(variables_.size()));
         }
 
-        template<class T_>
-        std::unique_ptr<Evaluator_<T_>> BuildFuzzyEvaluator(const size_t maxNestedIfs, const double defEps) const {
-            return std::unique_ptr<Evaluator_<T_>>(new FuzzyEvaluator_<T_>(variables_.size(), maxNestedIfs, defEps));
-        }
+//        template<class T_>
+//        std::unique_ptr<Evaluator_<T_>> BuildFuzzyEvaluator(const size_t maxNestedIfs, const double defEps) const {
+//            return std::unique_ptr<Evaluator_<T_>>(new FuzzyEvaluator_<T_>(variables_.size(), maxNestedIfs, defEps));
+//        }
 
         template<class T_>
         std::unique_ptr<Scenario_<T_>> BuildScenario() const {
@@ -59,7 +61,13 @@ namespace Dal::Script {
             }
         }
 
-        void Visit(Visitor_& v);
+        template <class V_>
+        void Visit(V_& v) {
+            for (auto &evt: events_) {
+                for (auto &stat: evt)
+                    v.Visit(stat);
+            }
+        }
 
         template<class T_>
         void Evaluate(const Scenario_<T_> &scen, Evaluator_<T_> &eval) const {
@@ -68,14 +76,14 @@ namespace Dal::Script {
             for (size_t i = 0; i < events_.size(); ++i) {
                 eval.SetCurEvt(i);
                 for (auto &statIt: events_[i])
-                    eval.VisitTree(statIt);
+                    eval.Visit(statIt);
             }
         }
 
         void IndexVariables();
-        size_t IFProcess();
-        void DomainProcess(bool fuzzy);
-        void ConstCondProcess();
+//        size_t IFProcess();
+//        void DomainProcess(bool fuzzy);
+//        void ConstCondProcess();
 
         size_t PreProcess(bool fuzzy, bool skipDoms);
         void Debug(std::ostream &ost);
