fde5e0660e6095c55b0762564a9d7ce49ba4fb04
wegamekinglc <scrappedprince.li@gmail.com>
2022-11-26 22:52:55 +0800

    update example

--- public/python/examples/dal_101.py
+++ public/python/examples/dal_101.py
@@ -1,12 +1,39 @@
 from dal import *
+import datetime as dt
+from datetime import timedelta
+
+
+def to_dal_date(date: dt.date):
+    return Date_(date.year, date.month, date.day)
+
 
 EvaluationDate_Set(Date_(2022, 9, 25))
-d = Date_(2025, 9, 25)
-dates = [d]
-events = ["call pays MAX(spot() - 120, 0.0)"]
-product = Product_New(dates, events)
+start_date = dt.date(2022, 9, 25)
+maturity = dt.date(2025, 9, 25)
+
+current_date = start_date
+dates = []
+while current_date < maturity:
+    dates.append(current_date)
+    current_date = current_date + timedelta(weeks=1)
+dates.append(maturity)
+
+event_dates = []
+events = []
+
+event_dates.append(to_dal_date(dates[0]))
+events.append("alive = 1")
+
+for d in dates[1:]:
+    event_dates.append(to_dal_date(d))
+    events.append("if spot() >= 150:1 then alive = 0 endif")
+
+event_dates.append(to_dal_date(dates[-1]))
+events.append("uoc pays alive * MAX(spot() - 120, 0.0)")
+
+product = Product_New(event_dates, events)
 model = BSModelData_New(100, 0.15, 0.0, 0.0)
-res = MonteCarlo_Value(product, model, 2 ** 20, "sobol", False, True, 0.01)
+res = MonteCarlo_Value(product, model, 2 ** 20, "sobol", False, True, 1.0)
 
 for k, v in res.items():
     print(f"{k}: {v}")
