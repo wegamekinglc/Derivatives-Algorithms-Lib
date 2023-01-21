ab6dcf9fdbaedea43f473c5e9557a3d28e064244
wegamekinglc <scrappedprince.li@gmail.com>
2023-01-21 11:55:06 +0800

    added rapidjson

--- dal/storage/json.cpp
+++ dal/storage/json.cpp
@@ -1,3 +1,4 @@
 //
 // Created by wegam on 2023/1/21.
 //
+
--- dal/storage/json.hpp
+++ dal/storage/json.hpp
@@ -3,3 +3,16 @@
 //
 
 #pragma once
+#include <dal/platform/platform.hpp>
+
+namespace Dal {
+    class String_;
+    class Storable_;
+
+    namespace JSON {
+        Handle_<Storable_> ReadString(const String_& src, bool quiet);
+        Handle_<Storable_> ReadFile(const String_& filename, bool quiet);
+        void WriteFile(const Storable_& object, const String_& filename);
+        String_ WriteString(const Storable_& object);
+    }
+}
