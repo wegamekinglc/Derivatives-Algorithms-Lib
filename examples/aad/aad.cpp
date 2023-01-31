01e2d24750b2322a8ef0808dd49d0cd93160a3cf
wegamekinglc <scrappedprince.li@gmail.com>
2023-02-01 00:28:45 +0800

    update aad example

--- examples/aad/aad.cpp
+++ examples/aad/aad.cpp
@@ -2,31 +2,39 @@
 // Created by wegam on 2020/12/21.
 //
 
+#define ADEPT_FAST
+#define ADEPT_FAST_EXPONENTIAL 1
+
 #include <dal/math/operators.hpp>
 #include <dal/math/aad/aad.hpp>
 #include <dal/math/vectors.hpp>
 #include <dal/utilities/timer.hpp>
 #include <iomanip>
 #include <iostream>
+#include <adept.h>
+#include <adept_source.h>
 
 using namespace std;
 using namespace Dal;
 using Dal::AAD::Number_;
 using Dal::AAD::Tape_;
+using adept::adouble;
 
 
 template <class T_>
 T_ BlackTest(T_ fwd, T_ vol, T_ numeraire, T_ strike, T_ expiry, bool is_call, int n_repetition) {
+    static const double M_SQRT_2 = 1.4142135623730951;
     const double omega = is_call ? 1.0 : -1.0;
     T_ y(0.0);
     for(int i = 0; i < n_repetition; ++i) {
-        const auto sqrt_var = vol * sqrt(expiry);
-        const auto dMinus = log(fwd / strike) / sqrt_var - 0.5 * sqrt_var;
-        const auto dPlus = dMinus + sqrt_var;
-        y += numeraire * omega * (fwd * NCDF(dPlus) - strike * NCDF(dMinus));
+        T_ sqrt_var = vol * sqrt(expiry);
+        T_ d_minus = log(fwd / strike) / sqrt_var - 0.5 * sqrt_var;
+        T_ d_plus = d_minus + sqrt_var;
+        y += numeraire * omega * (0.5 * fwd * erfc(-d_plus / M_SQRT_2) - strike * 0.5 * erfc(-d_minus / M_SQRT_2));
     }
     return y;
 }
+
 
 
 int main() {
@@ -86,11 +94,31 @@ int main() {
         tape.ResetAdjoints();
     }
 
-    std::cout << "   AAD Mode: " << std::setprecision(8) << price << " with " << timer.Elapsed<milliseconds>() << " ms" << std::endl;
-    std::cout << "    dP/dFwd: " << std::setprecision(8) << d_fwd_aad << std::endl;
-    std::cout << "    dP/dVol: " << std::setprecision(8) << d_vol_aad << std::endl;
-    std::cout << "    dP/dNum: " << std::setprecision(8) << d_numeraire_aad << std::endl;
-    std::cout << "    dP/dK  : " << std::setprecision(8) << d_strike_aad << std::endl;
-    std::cout << "    dP/dT  : " << std::setprecision(8) << d_expiry_aad << std::endl;
+    std::cout << " DAL   AAD Mode: " << std::setprecision(8) << price << " with " << timer.Elapsed<milliseconds>() << " ms" << std::endl;
+    std::cout << "       dP/dFwd : " << std::setprecision(8) << d_fwd_aad << std::endl;
+    std::cout << "       dP/dVol : " << std::setprecision(8) << d_vol_aad << std::endl;
+    std::cout << "       dP/dNum : " << std::setprecision(8) << d_numeraire_aad << std::endl;
+    std::cout << "       dP/dK   : " << std::setprecision(8) << d_strike_aad << std::endl;
+    std::cout << "       dP/dT   : " << std::setprecision(8) << d_expiry_aad << std::endl;
+    tape.Clear();
+
+    timer.Reset();
+    double y_value;
+    adept::Stack stack;
+    adouble x[5] = {fwd, vol, numeraire, strike, expiry};
+    for (int i = 0; i < n_rounds; ++i) {
+        stack.new_recording();
+        adouble y = BlackTest(x[0], x[1], x[2], x[3], x[4], is_call, n_repetition);
+        y.set_gradient(1.0); // Defines y as the cost function
+        stack.compute_adjoint();
+        y_value = y.value();
+    }
+    std::cout << "ADEPT AAD Mode: " << std::setprecision(8) << y_value << " with " << timer.Elapsed<milliseconds>() << " ms" << std::endl;
+    std::cout << "      dP/dFwd : " << std::setprecision(8) << x[0].get_gradient() << std::endl;
+    std::cout << "      dP/dVol : " << std::setprecision(8) << x[1].get_gradient() << std::endl;
+    std::cout << "      dP/dNum : " << std::setprecision(8) << x[2].get_gradient() << std::endl;
+    std::cout << "      dP/dK   : " << std::setprecision(8) << x[3].get_gradient() << std::endl;
+    std::cout << "      dP/dT   : " << std::setprecision(8) << x[4].get_gradient() << std::endl;
+
     return 0;
 }
