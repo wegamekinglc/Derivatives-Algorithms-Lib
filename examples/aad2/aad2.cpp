eaf3c13c02ccff49ac13e72a1e2f74a359184eee
wegamekinglc <scrappedprince.li@gmail.com>
2023-02-04 20:34:28 +0800

    small change to aad2

--- examples/aad2/aad2.cpp
+++ examples/aad2/aad2.cpp
@@ -39,8 +39,8 @@ using namespace Dal;
 using adept::adouble;
 
 int main() {
-    int n_rounds = 100;
-    int n_repetition = 1;
+    int n_rounds = 10;
+    int n_repetition = 10000000;
     double fwd = 1.00;
     double vol = 0.20;
     double numeraire = 1.0;
@@ -48,7 +48,6 @@ int main() {
     double expiry = 3.0;
     bool is_call = true;
 
-    double price;
     Number_ spot_aad(2.0 * fwd);
     Number_ vol_aad(vol);
     Number_ numeraire_aad(numeraire);
@@ -70,72 +69,50 @@ int main() {
     Number_ fwd_aad = spot_aad / 2.0;
     tape.Mark();
 
-    double d_spot_aad;
-    double d_fwd_aad;
-    double d_vol_aad;
-    double d_numeraire_aad;
-    double d_strike_aad;
-    double d_expiry_aad;
-
     Number_ price_aad;
-
     for (int i = 0; i < n_rounds; ++i) {
         tape.RewindToMark();
         price_aad = BlackTest(fwd_aad, vol_aad, numeraire_aad, strike_aad, expiry_aad, is_call, n_repetition);
         price_aad.PropagateToMark();
-
-        price = price_aad.Value() / n_repetition;
-        d_fwd_aad = fwd_aad.Adjoint() / n_repetition / n_rounds;
-        d_vol_aad = vol_aad.Adjoint() / n_repetition / n_rounds;
-        d_numeraire_aad = numeraire_aad.Adjoint() / n_repetition / n_rounds;
-        d_strike_aad = strike_aad.Adjoint() / n_repetition / n_rounds;
-        d_expiry_aad = expiry_aad.Adjoint() / n_repetition / n_rounds;
     }
     price_aad.PropagateMarkToStart();
-    d_spot_aad = spot_aad.Adjoint() / n_repetition / n_rounds;
-    std::cout << " DAL  AAD Mode: " << std::setprecision(8) << price << " with " << timer.Elapsed<milliseconds>() << " ms" << std::endl;
-    std::cout << "      dP/dSpt : " << std::setprecision(8) << d_spot_aad << std::endl;
-    std::cout << "      dP/dFwd : " << std::setprecision(8) << d_fwd_aad << std::endl;
-    std::cout << "      dP/dVol : " << std::setprecision(8) << d_vol_aad << std::endl;
-    std::cout << "      dP/dNum : " << std::setprecision(8) << d_numeraire_aad << std::endl;
-    std::cout << "      dP/dK   : " << std::setprecision(8) << d_strike_aad << std::endl;
-    std::cout << "      dP/dT   : " << std::setprecision(8) << d_expiry_aad << std::endl;
+    std::cout << " DAL  AAD Mode: " << std::setprecision(8) << price_aad.Value() / n_repetition << " with " << timer.Elapsed<milliseconds>() << " ms" << std::endl;
+    std::cout << "      dP/dSpt : " << std::setprecision(8) << spot_aad.Adjoint() / n_repetition / n_rounds << std::endl;
+    std::cout << "      dP/dFwd : " << std::setprecision(8) << fwd_aad.Adjoint() / n_repetition / n_rounds << std::endl;
+    std::cout << "      dP/dVol : " << std::setprecision(8) << vol_aad.Adjoint() / n_repetition / n_rounds << std::endl;
+    std::cout << "      dP/dNum : " << std::setprecision(8) << numeraire_aad.Adjoint() / n_repetition / n_rounds << std::endl;
+    std::cout << "      dP/dK   : " << std::setprecision(8) << strike_aad.Adjoint() / n_repetition / n_rounds << std::endl;
+    std::cout << "      dP/dT   : " << std::setprecision(8) << expiry_aad.Adjoint() / n_repetition / n_rounds << std::endl;
     tape.Clear();
 
     timer.Reset();
     double y_value;
-    adept::Stack stack1;
-    adept::Stack stack2(false);
-    stack1.new_recording();
+    adept::Stack stack;
     adouble spot_adept = 2.0 * fwd;
     adouble fwd_adept = spot_adept / 2.0;
-    stack1.deactivate();
 
-    stack2.activate();
-    adouble x[5] = {fwd_adept.value(), vol, numeraire, strike, expiry};
+    adouble x[5] = {fwd_adept, vol, numeraire, strike, expiry};
     for (int i = 0; i < n_rounds; ++i) {
-        stack2.new_recording();
+        stack.new_recording();
         adouble y = BlackTest(x[0], x[1], x[2], x[3], x[4], is_call, n_repetition);
         y.set_gradient(1.0); // Defines y as the cost function
-        stack2.compute_adjoint();
+        stack.compute_adjoint();
         y_value = y.value();
     }
 
-    std::cout << "ADEPT AAD Mode: " << std::setprecision(8) << y_value << " with " << timer.Elapsed<milliseconds>() << " ms" << std::endl;
-//    std::cout << "      dP/dSpt : " << std::setprecision(8) << spot_adept.get_gradient() << std::endl;
-    std::cout << "      dP/dFwd : " << std::setprecision(8) << x[0].get_gradient() << std::endl;
-    std::cout << "      dP/dVol : " << std::setprecision(8) << x[1].get_gradient() << std::endl;
-    std::cout << "      dP/dNum : " << std::setprecision(8) << x[2].get_gradient() << std::endl;
-    std::cout << "      dP/dK   : " << std::setprecision(8) << x[3].get_gradient() << std::endl;
-    std::cout << "      dP/dT   : " << std::setprecision(8) << x[4].get_gradient() << std::endl;
+    std::cout << "ADEPT AAD Mode: " << std::setprecision(8) << y_value / n_repetition << " with " << timer.Elapsed<milliseconds>() << " ms" << std::endl;
+    std::cout << "      dP/dFwd : " << std::setprecision(8) << x[0].get_gradient() / n_repetition << std::endl;
+    std::cout << "      dP/dVol : " << std::setprecision(8) << x[1].get_gradient() / n_repetition << std::endl;
+    std::cout << "      dP/dNum : " << std::setprecision(8) << x[2].get_gradient() / n_repetition << std::endl;
+    std::cout << "      dP/dK   : " << std::setprecision(8) << x[3].get_gradient() / n_repetition << std::endl;
+    std::cout << "      dP/dT   : " << std::setprecision(8) << x[4].get_gradient() / n_repetition << std::endl;
 
     double fwd_adept_val = x[0].get_gradient();
-    stack2.deactivate();
-    stack1.activate();
-    stack1.continue_recording();
+    stack.new_recording();
+    fwd_adept = spot_adept / 2.0;
     fwd_adept.set_gradient(fwd_adept_val);
-    stack1.compute_adjoint();
-    std::cout << "      dP/dSpt : " << std::setprecision(8) << spot_adept.get_gradient() << std::endl;
+    stack.compute_adjoint();
+    std::cout << "      dP/dSpt : " << std::setprecision(8) << spot_adept.get_gradient() / n_repetition << std::endl;
 
     return 0;
 }
