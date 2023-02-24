2090ed78ba282f2e1a42b5eb728a76aff233a402
wegamekinglc <scrappedprince.li@gmail.com>
2023-02-25 02:04:48 +0800

    std math include

--- dal/math/pde/pde.cpp
+++ dal/math/pde/pde.cpp
@@ -2,6 +2,7 @@
 // Created by wegam on 2023/2/24.
 //
 
+#include <cmath>
 #include <dal/math/pde/pde.hpp>
 #include <dal/utilities/algorithms.hpp>
 #include <dal/platform/strict.hpp>
@@ -22,9 +23,9 @@ namespace Dal::PDE {
             double lambda_;
             SinhMap_(double lambda) : lambda_(lambda) {}
             double operator()(double y, double* xp = nullptr, double* xpp = nullptr) const override {
-                ASSIGN(xp, cosh(y / lambda_));
-                ASSIGN(xpp, sinh(y / lambda_) / lambda_);
-                return lambda_ * sinh(y / lambda_);
+                ASSIGN(xp, std::cosh(y / lambda_));
+                ASSIGN(xpp, std::sinh(y / lambda_) / lambda_);
+                return lambda_ * std::sinh(y / lambda_);
             }
             double Y(double x) const override { return lambda_ * asinh(x / lambda_); }
         };
@@ -32,7 +33,7 @@ namespace Dal::PDE {
 
     PDE::CoordinateMap_* PDE::NewSinhMap(double x_width, double dxdy_range) {
         REQUIRE(IsPositive(x_width) && dxdy_range >= 1.0, "x_width should be positive and dxdy_range should be greater than 1");
-        double sinhMaxY = sqrt(Square(dxdy_range) - 1.0);
+        double sinhMaxY = std::sqrt(Square(dxdy_range) - 1.0);
         return IsZero(Square(sinhMaxY)) ? (CoordinateMap_*)new IdentityMap_ : new SinhMap_(x_width / sinhMaxY);
     }
 } // namespace Dal::PDE
