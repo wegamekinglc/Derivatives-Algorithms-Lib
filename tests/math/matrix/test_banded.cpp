a07504597f6f6502bba180b18f5a2a77a1ece829
wegamekinglc <scrappedprince.li@gmail.com>
2023-03-03 00:02:11 +0800

    added tests for tridiag_

--- tests/math/matrix/test_banded.cpp
+++ tests/math/matrix/test_banded.cpp
@@ -22,7 +22,6 @@ TEST(BandedTest, TestNewBandedDiagonal) {
     ASSERT_THROW(mat->Set(0, 2, 2.0), Exception_);
 }
 
-
 /*
  * TODO: make the tests meaningful
  */
@@ -38,11 +37,74 @@ TEST(BandedTest, TestNewBandedBanded) {
     ASSERT_EQ((*mat)(9, 8), 3.0);
 }
 
-
 TEST(BandedTest, TestLowerBandAccumulator) {
     LowerBandAccumulator_ acc(3, 1);
     auto offset = 0;
     Vector_<> v_in{1.0, 2.0};
     acc.Add(v_in, offset);
     acc.Add(2.0 * v_in, offset);
+}
+
+TEST(BandedTest, TestTriDiagonalMultiply) {
+    const auto n = 10;
+    Sparse::TriDiagonal_ trig(n);
+    for(int i = 0; i < n; ++i) {
+        if (i == 0) {
+            trig.Set(i, i, 5.0);
+            trig.Set(i, i + 1, 2.0);
+        }
+        else if (i == 9) {
+            trig.Set(i, i, 5.0);
+            trig.Set(i, i - 1, 1.0);
+        }
+        else {
+            trig.Set(i, i, 5.0);
+            trig.Set(i, i - 1, 1.0);
+            trig.Set(i, i + 1, 2.0);
+        }
+    }
+
+    Vector_<> v(10, 1.0);
+    Vector_<> expected = {7.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 6.0};
+    Vector_<> calculated(n);
+    trig.MultiplyLeft(v, &calculated);
+    for (int i = 0; i < n; ++i)
+        ASSERT_DOUBLE_EQ(calculated[i], expected[i]);
+
+    trig.MultiplyRight(v, &calculated);
+    expected = {6.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 7.0};
+    for (int i = 0; i < n; ++i)
+        ASSERT_DOUBLE_EQ(calculated[i], expected[i]);
+}
+
+TEST(BandedTest, TestTriDiagonalSolve) {
+    const auto n = 10;
+    Sparse::TriDiagonal_ trig(n);
+    for(int i = 0; i < n; ++i) {
+        if (i == 0) {
+            trig.Set(i, i, 5.0);
+            trig.Set(i, i + 1, 2.0);
+        }
+        else if (i == 9) {
+            trig.Set(i, i, 5.0);
+            trig.Set(i, i - 1, 1.0);
+        }
+        else {
+            trig.Set(i, i, 5.0);
+            trig.Set(i, i - 1, 1.0);
+            trig.Set(i, i + 1, 2.0);
+        }
+    }
+
+    Vector_<> v(10, 1.0);
+    Vector_<> expected = {0.15237329, 0.11906678, 0.1261464 , 0.1251006 , 0.1241753 , 0.12701146, 0.12038371, 0.135535 , 0.10097065, 0.17980587};
+    Vector_<> calculated(n);
+    trig.Decompose()->SolveLeft(v, &calculated);
+    for (int i = 0; i < n; ++i)
+        ASSERT_NEAR(calculated[i], expected[i], 1e-6);
+
+    trig.Decompose()->SolveRight(v, &calculated);
+    expected = {0.17980587, 0.10097065, 0.135535  , 0.12038371, 0.12701146, 0.1241753 , 0.1251006 , 0.1261464 , 0.11906678, 0.15237329};
+    for (int i = 0; i < n; ++i)
+        ASSERT_NEAR(calculated[i], expected[i], 1e-6);
 }
