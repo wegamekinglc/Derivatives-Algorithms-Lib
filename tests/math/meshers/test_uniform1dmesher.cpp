a07880658c82efb641890f4db63ad16b80f29f7c
wegamekinglc <scrappedprince.li@gmail.com>
2023-03-18 16:51:20 +0800

    added tests for uniform1dmesher tests

--- tests/math/meshers/test_uniform1dmesher.cpp
+++ tests/math/meshers/test_uniform1dmesher.cpp
@@ -8,6 +8,9 @@
 
 namespace Dal {
     TEST(MeshersTest, TestUniform1DMesher) {
-
+        const int n = 51;
+        Uniform1DMesher_ x(0.0, 5.0, n);
+        ASSERT_NEAR(x.DPlus(0), 0.1, 1e-8);
+        ASSERT_NEAR(x.Location(n - 1), 5.0, 1e-8);
     }
 }
