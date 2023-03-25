d778d902efc5e78f800fd3733b126d49c9a49a0d
wegamekinglc <scrappedprince.li@gmail.com>
2023-03-25 20:35:35 +0800

    change folder

--- tests/math/pde/meshers/test_uniform1dmesher.cpp
+++ tests/math/pde/meshers/test_uniform1dmesher.cpp
@@ -3,13 +3,13 @@
 //
 
 #include <gtest/gtest.h>
-#include <dal/math/pde/meshers/concentrating1dmesher.hpp>
+#include "dal/math/pde/meshers/uniform1dmesher.hpp"
 
 using namespace Dal;
 
-TEST(MeshersTest, TestConcentrating1dMesher) {
+TEST(MeshersTest, TestUniform1DMesher) {
     const int n = 51;
-    Concentrating1dMesher_ x(0.0, 5.0, n, std::make_pair(2.5, 1000));
+    Uniform1DMesher_ x(0.0, 5.0, n);
     ASSERT_NEAR(x.DPlus(0), 0.1, 1e-8);
     ASSERT_NEAR(x.Location(n - 1), 5.0, 1e-8);
 }
