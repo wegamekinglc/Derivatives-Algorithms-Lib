fc9ee6afb981e5db70ab860810dfaea003ddb33f
wegamekinglc <scrappedprince.li@gmail.com>
2023-09-02 23:03:19 +0800

    fixed

--- examples/snowball/snowball.cpp
+++ examples/snowball/snowball.cpp
@@ -35,9 +35,10 @@ int main() {
     const double ko = 1.00;
     const double ki = 0.88;
     const double coupon = 0.069;
+    const String_ freq = "1M";
     const Date_ maturity(2025, 3, 1);
     const Date_ start = Global::Dates_::EvaluationDate();
-    const int num_path = 8192;
+    const int num_path = std::pow(2, 20);
 
     timer.Reset();
 
@@ -57,7 +58,7 @@ int main() {
     // monitor
     eventDates.emplace_back(start);
     events.push_back("alive = 1 is_ki = 0");
-    eventDates.emplace_back("START: 2023-06-01 END: 2025-02-01 FREQ: 1M");
+    eventDates.emplace_back("START: 2023-06-01 END: 2025-02-01 FREQ: " + freq);
     auto dcf = "DCF(ACT365F, " + Date::ToString(start) + ", PeriodEnd)";
     events.push_back("if spot() < KI:0.001 then is_ki = 1 end\n"
                      "if spot() > KO:0.001 then call pays alive * COUPON * " + dcf + " alive = 0 end");
@@ -67,7 +68,7 @@ int main() {
                      "if spot() > KO:0.001 then call pays alive * COUPON * " + dcf + " alive = 0  end\n"
                      "call pays alive * is_ki * (spot() - STRIKE) + alive * (1.000000 - is_ki) * COUPON * " + dcf);
     ScriptProduct_ product(eventDates, events, "call");
-    product.Debug();
+    const int num_obs = freq == "1W" ? 3 * 51 : 3 * 12;
 
     Vector_<int> widths = {14, 14, 14, 14, 14, 14, 14, 14, 14};
 
@@ -94,7 +95,7 @@ int main() {
 
         std::cout << std::setw(widths[0]) << std::left << "Non-AAD"
                   << std::setw(widths[1]) << std::right << num_path
-                  << std::setw(widths[2]) << std::right << events.size()
+                  << std::setw(widths[2]) << std::right << num_obs
                   << std::fixed
                   << std::setprecision(6)
                   << std::setw(widths[3]) << std::right << calculated
@@ -118,7 +119,7 @@ int main() {
 
         std::cout << std::setw(widths[0]) << std::left << "AAD"
                   << std::setw(widths[1]) << std::right << num_path
-                  << std::setw(widths[2]) << std::right << events.size()
+                  << std::setw(widths[2]) << std::right << num_obs
                   << std::fixed
                   << std::setprecision(6)
                   << std::setw(widths[3]) << std::right << calculated
--- examples/uoc/uoc.cpp
+++ examples/uoc/uoc.cpp
@@ -37,6 +37,7 @@ int main() {
     const double div = 0.0;
     const double strike = 120.0;
     const double barrier = 150.0;
+    const String_ freq = "1W";
 
     timer.Reset();
 
@@ -48,38 +49,89 @@ int main() {
     events.push_back(ToString(barrier));
     eventDates.push_back(Cell_(start));
     events.push_back("alive = 1");
-    eventDates.push_back(Cell_("START: " + Date::ToString(start) + " END: " + Date::ToString(maturity) + " FREQ: 1W"));
+    eventDates.push_back(Cell_("START: " + Date::ToString(start) + " END: " + Date::ToString(maturity) + " FREQ: " + freq));
     events.push_back("if spot() >= BARRIER:0.1 then alive = 0 end");
     eventDates.push_back(Cell_(maturity));
     events.push_back(String_("if spot() >= BARRIER:0.1 then alive = 0 end\n call pays alive * MAX(spot() - STRIKE, 0.0)"));
 
     ScriptProduct_ product(eventDates, events);
+    const int num_obs = freq == "1W" ? 3 * 51 : 3 * 12;
 
     auto times = Vector::XRange(0.0, 5.0, 61);
     auto spots = Vector::XRange(50.0, 200.0, 31);
-    std::unique_ptr<Model_<Real_>> model = std::make_unique<Dupire_<Real_>>(Real_(spot),
-                                                                            Real_(rate),
-                                                                            Real_(div),
-                                                                            spots,
-                                                                            times,
-                                                                            Matrix_<Real_>(spots.size(),
-                                                                                           times.size(),
-                                                                                           Real_(vol)),
-                                                                            10.0);
-    std::cout << "\nParsing " << std::setprecision(8) << "\tElapsed: " << timer.Elapsed<milliseconds>() << " ms" << std::endl;
 
-    timer.Reset();
-    int max_nested_ifs = product.PreProcess(true, true);
-    SimResults_<Real_> results = MCSimulation(product, *model, std::pow(2, 20), String_("sobol"), false, max_nested_ifs);
+    Vector_<int> widths = {14, 14, 14, 14, 14, 14, 14, 14, 14, 14};
+    std::cout << std::setw(widths[0]) << std::left << "Method"
+              << std::setw(widths[1]) << std::right << "# of paths"
+              << std::setw(widths[2]) << std::right << "# of obs"
+              << std::setw(widths[3]) << std::right << "PV"
+              << std::setw(widths[4]) << std::right << "delta"
+              << std::setw(widths[5]) << std::right << "dP/dR"
+              << std::setw(widths[6]) << std::right << "dP/dDiv"
+              << std::setw(widths[7]) << std::right << "vega"
+              << std::setw(widths[8]) << std::right << "dP/dK"
+              << std::setw(widths[9]) << std::right << "Elapsed (ms)"
+              << std::endl;
+    {
+        std::unique_ptr<Model_<double>> model = std::make_unique<Dupire_<double>>(spot,
+                                                                                  rate,
+                                                                                  div,
+                                                                                  spots,
+                                                                                  times,
+                                                                                  Matrix_<double>(spots.size(),times.size(),vol),
+                                                                                  10.0);
+        timer.Reset();
 
-    auto calculated = results.aggregated_ / static_cast<double>(std::pow(2, 20));
-    std::cout << "\nEuropean       w. Dupire: price " << std::setprecision(8) << calculated << "\tElapsed: " << timer.Elapsed<milliseconds>() << " ms" << std::endl;
-    auto vega = 0.0;
-    for (int i = 3; i < results.risks_.size(); ++i)
-        vega += results.risks_[i];
-    std::cout << "                     : delta " << std::setprecision(8) << results.risks_[0] << std::endl;
-    std::cout << "                     : vega  " << std::setprecision(8) << vega << std::endl;
-    std::cout << "                     : rho   " << std::setprecision(8) << results.risks_[1] << std::endl;
+        int max_nested_ifs = product.PreProcess(false, false);
+        const int num_path = std::pow(2, 20);
+        SimResults_<double> results = MCSimulation(product, *model, num_path, String_("sobol"), false);
 
+        auto calculated = results.aggregated_ / static_cast<double>(num_path);
+
+        std::cout << std::setw(widths[0]) << std::left << "Non-AAD"
+                  << std::setw(widths[1]) << std::right << num_path
+                  << std::setw(widths[2]) << std::right << num_obs
+                  << std::fixed << std::setprecision(6)
+                  << std::setw(widths[3]) << std::right << calculated
+                  << std::setw(widths[4]) << std::right << "#NA"
+                  << std::setw(widths[5]) << std::right << "#NA"
+                  << std::setw(widths[6]) << std::right << "#NA"
+                  << std::setw(widths[7]) << std::right << "#NA"
+                  << std::setw(widths[8]) << std::right << "#NA"
+                  << std::setw(widths[9]) << std::right << int(timer.Elapsed<milliseconds>()) << std::endl;
+    }
+
+    {
+        std::unique_ptr<Model_<Real_>> model = std::make_unique<Dupire_<Real_>>(Real_(spot),
+                                                                                Real_(rate),
+                                                                                Real_(div),
+                                                                                spots,
+                                                                                times,
+                                                                                Matrix_<Real_>(spots.size(),times.size(),Real_(vol)),
+                                                                                10.0);
+        timer.Reset();
+
+        int max_nested_ifs = product.PreProcess(true, true);
+        const int num_path = std::pow(2, 20);
+        SimResults_<Real_> results = MCSimulation(product, *model, num_path, String_("sobol"), false, max_nested_ifs);
+
+        auto calculated = results.aggregated_ / static_cast<double>(num_path);
+        const int vol_length = 31 * 61;
+        double vega = 0.0;
+        for (auto i = 3; i < 3 + vol_length; ++i)
+            vega += results.risks_[i];
+
+        std::cout << std::setw(widths[0]) << std::left << "AAD"
+                  << std::setw(widths[1]) << std::right << num_path
+                  << std::setw(widths[2]) << std::right << num_obs
+                  << std::fixed << std::setprecision(6)
+                  << std::setw(widths[3]) << std::right << calculated
+                  << std::setw(widths[4]) << std::right << results.risks_[0]
+                  << std::setw(widths[5]) << std::right << results.risks_[1]
+                  << std::setw(widths[6]) << std::right << results.risks_[2]
+                  << std::setw(widths[7]) << std::right << vega
+                  << std::setw(widths[8]) << std::right << results.risks_[3 + vol_length]
+                  << std::setw(widths[9]) << std::right << int(timer.Elapsed<milliseconds>()) << std::endl;
+    }
     return 0;
 }
