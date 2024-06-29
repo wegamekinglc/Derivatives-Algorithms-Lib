2e5e23cf4a344f606f2d0984510ca4e5b9bf4a5d
wegamekinglc <scrappedprince.li@gmail.com>
2024-06-30 00:42:29 +0800

    added accrualperiod tests

--- tests/protocol/test_accrualperiod.cpp
+++ tests/protocol/test_accrualperiod.cpp
@@ -12,7 +12,9 @@ TEST(ProtocolTest, TestAccrualPeriod) {
     Date_ start(2023, 5, 21);
     Date_ end(2023, 8, 21);
     AccrualPeriod_ ap(start, end, 1.0, DayBasis_("Act_365F"));
+    ASSERT_NEAR(ap.dcf_, 0.1, 0.25205479452054796);
 
     Handle_<DayBasis::Context_> ctx(new DayBasis::Context_(false, start, end, 12));
     AccrualPeriod_ ap2(start, end, 1.0, DayBasis_("Act_365F"), ctx, false);
+    ASSERT_NEAR(ap2.dcf_, 0.1, 0.25205479452054796);
 }
