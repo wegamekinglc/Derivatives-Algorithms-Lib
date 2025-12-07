f0877c28e6271e9d22acaa1d00206f102a59c1b8
wegamekinglc <scrappedprince.li@gmail.com>
2025-12-07 20:36:29 +0800

    added example for dates

--- examples/date_utilities/date_utilities.cpp
+++ examples/date_utilities/date_utilities.cpp
@@ -3,8 +3,10 @@
 //
 
 #include <iostream>
+#include <format>
 #include <dal/platform/platform.hpp>
 #include <dal/time/date.hpp>
+#include <dal/time/holidays.hpp>
 
 using namespace Dal;
 using namespace std;
@@ -12,8 +14,18 @@ using namespace std;
 int main() {
     Dal::RegisterAll_::Init();
 
-    Date_ d(2018, 8, 8);
-    cout << Date::ToString(d) << endl;
+    Date_ d(2025, 12, 6);
+    cout << std::format("This date: {0}", Date::ToString(d).c_str()) << endl;
+
+    Holidays_ hol("CN.SSE CN.IB");
+    Date_ next_d = Holidays::NextBus(hol, d);
+    cout << std::format("Next {0} business date: {1}", hol.String().c_str(), Date::ToString(next_d).c_str()) << endl;
+
+    Date_ prev_d = Holidays::PrevBus(hol, d);
+    cout << std::format("Prev {0} business date: {1}", hol.String().c_str(), Date::ToString(prev_d).c_str()) << endl;
+
+    CountBusDays_ counter(hol);
+    cout << std::format("Business days to year end: {0}", counter(d, Date_(2025, 12, 31)));
 
     return 0;
 }
