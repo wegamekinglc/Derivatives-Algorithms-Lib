679359a47f2ac5152656e8f8a3c1305c470bf724
wegamekinglc <scrappedprince.li@gmail.com>
2025-12-07 19:59:29 +0800

    update example

--- examples/sobol/sobol.cpp
+++ examples/sobol/sobol.cpp
@@ -17,17 +17,19 @@ int main() {
     Dal::RegisterAll_::Init();
 
     Timer_ timer;
-    Vector_<int> widths = {20, 14, 14};
+    Vector_<int> widths = {20, 14, 14, 14};
 
     std::cout << std::setw(widths[0]) << std::right << "# of paths"
-              << std::setw(widths[1]) << std::right << "Uniform"
-              << std::setw(widths[2]) << std::right << "Normal"
+              << std::setw(widths[1]) << std::right << "# of dims"
+              << std::setw(widths[2]) << std::right << "Uniform"
+              << std::setw(widths[3]) << std::right << "Normal"
               << std::endl;
 
     Vector_<int> p_num_paths = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+    const int num_dims  = 5;
     for (auto i: p_num_paths) {
         Vector_<> dst;
-        unique_ptr<SequenceSet_> rsg(NewSobol(1, 1000));
+        unique_ptr<SequenceSet_> rsg(NewSobol(num_dims, 1000));
         const auto num_paths = static_cast<size_t>(std::pow(2, i));
 
         timer.Reset();
@@ -44,8 +46,9 @@ int main() {
         std::cout << std::fixed
                   << std::setprecision(6)
                   << std::setw(widths[0]) << std::right << num_paths
-                  << std::setw(widths[1]) << std::right << uniform_duration
-                  << std::setw(widths[2]) << std::right << normal_duration
+                  << std::setw(widths[1]) << std::right << num_dims
+                  << std::setw(widths[2]) << std::right << uniform_duration
+                  << std::setw(widths[3]) << std::right << normal_duration
                   << std::endl;
 
     }
